0032e84f5046de72a17eb76df095744683073b75
Colin Nelson <orthros@users.noreply.github.com>
2019-07-25 12:19:47 -0700

    feat: added utils for running probot apps in gcf (#2)

---
+++ packages/gcf-utils/src/gcf-utils.ts
@@ -0,0 +1,113 @@
+/**
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { createProbot, Probot, ApplicationFunction, Options, Application } from 'probot';
+import { resolve } from 'probot/lib/resolver';
+import { Storage } from '@google-cloud/storage';
+import * as KMS from '@google-cloud/kms';
+import { readFileSync } from 'fs';
+import * as express from 'express';
+
+
+export class GCFBootstrapper {
+  probot?: Probot;
+
+  async loadProbot(appFn: ApplicationFunction): Promise<Probot> {
+    if (!this.probot) {
+      const cfg = await this.getProbotConfig();
+      this.probot = createProbot(cfg);
+    }
+
+    if (typeof appFn === 'string') {
+      appFn = resolve(appFn)
+    }
+
+    this.probot.load(appFn)
+
+    return this.probot
+  }
+
+  async getProbotConfig(): Promise<Options> {
+
+    const storage = new Storage();
+    const kmsclient = new KMS.KeyManagementServiceClient();
+
+    const destFileName = "/tmp/creds.json";
+    const bucketName = process.env.DRIFT_PRO_BUCKET || '';
+    const srcFilename = process.env.GCF_SHORT_FUNCTION_NAME || '';
+
+    const options = {
+      destination: destFileName,
+    };
+
+    // Downloads the file
+    await storage.bucket(bucketName)
+      .file(srcFilename)
+      .download(options);
+
+
+    const contentsBuffer = readFileSync(destFileName);
+    const name = kmsclient.cryptoKeyPath(
+      process.env.PROJECT_ID || '',
+      process.env.KEY_LOCATION || '',
+      process.env.KEY_RING || '',
+      process.env.GCF_SHORT_FUNCTION_NAME || ''
+    );
+
+    const ciphertext = contentsBuffer.toString('base64');
+
+    // Decrypts the file using the specified crypto key
+    const [result] = await kmsclient.decrypt({ name, ciphertext });
+
+    const config = JSON.parse(result.plaintext.toString());
+    return config
+  }
+
+  async gcf(appFn: ApplicationFunction): Promise<(request: express.Request, response: express.Response) => Promise<void>> {
+    return async (request: express.Request, response: express.Response) => {
+      // Otherwise let's listen handle the payload
+      this.probot = this.probot || await this.loadProbot(appFn);
+
+      // Determine incoming webhook event type
+      const name = request.get('x-github-event') || request.get('X-GitHub-Event');
+      const id = request.get('x-github-delivery') || request.get('X-GitHub-Delivery') || '';
+
+      // Do the thing
+      if (name) {
+        try {
+          await this.probot.receive({
+            name,
+            id,
+            payload: request.body
+          });
+          response.send({
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Executed' })
+          });
+        } catch (err) {
+          console.error(err)
+          response.send({
+            statusCode: 500,
+            body: JSON.stringify({ message: err })
+          });
+        }
+      } else {
+        console.error(request);
+        response.sendStatus(400);
+      }
+    }
+  }
+}
---
+++ packages/gcf-utils/test/gcf-utils.ts
@@ -0,0 +1,147 @@
+/**
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { GCFBootstrapper } from '../src/gcf-utils';
+
+import { GitHubAPI } from 'probot/lib/github';
+import { Options } from 'probot';
+import * as express from 'express';
+import sinon from 'sinon';
+
+
+describe('GCFBootstrapper', () => {
+
+    describe("gcf", () => {
+        let handler: (request: express.Request, response: express.Response) => Promise<void>;
+
+        let response: express.Response = express.response;
+        let sendStub: sinon.SinonStub<[any?], express.Response> = sinon.stub(response, "send");
+        let sendStatusStub: sinon.SinonStub<[number], express.Response> = sinon.stub(response, "sendStatus");
+
+        let req: express.Request;
+
+        let spy: sinon.SinonStub = sinon.stub();
+        let configStub: sinon.SinonStub<[], Promise<Options>>;
+
+        let bootstrapper: GCFBootstrapper;
+
+        beforeEach(async () => {
+            req = express.request;
+
+            bootstrapper = new GCFBootstrapper();
+            configStub = sinon.stub(bootstrapper, "getProbotConfig").callsFake(() => {
+                return Promise.resolve({ id: 1234, secret: "foo", webhookPath: "bar" })
+            });
+
+            handler = await bootstrapper.gcf(async app => {
+                app.auth = () => new Promise<GitHubAPI>((resolve, reject) => {
+                    resolve(GitHubAPI());
+                })
+                app.on('issues', spy);
+                app.on('err', sinon.stub().throws());
+            });
+        });
+
+        afterEach(() => {
+            sendStub.reset();
+            sendStatusStub.reset();
+            spy.reset();
+            configStub.reset();
+        });
+
+        it('calls the event handler', async () => {
+            req.body = {
+                installation: { id: 1 }
+            }
+            req.headers = {};
+            req.headers['x-github-event'] = 'issues';
+            req.headers['x-github-delivery'] = '123';
+
+            await handler(req, response)
+
+            sinon.assert.calledOnce(configStub);
+            sinon.assert.notCalled(sendStatusStub);
+            sinon.assert.calledOnce(sendStub);
+            sinon.assert.calledOnce(spy);
+        });
+
+        it('does nothing if there are missing headers', async () => {
+            req.body = {
+                installation: { id: 1 }
+            };
+            req.headers = {};
+
+            await handler(req, response);
+
+            sinon.assert.calledOnce(configStub);
+            sinon.assert.notCalled(spy);
+            sinon.assert.notCalled(sendStub);
+            sinon.assert.calledWith(sendStatusStub, 400);
+        });
+
+
+        it('returns 500 on errors', async () => {
+            req.body = {
+                installtion: { id: 1 }
+            };
+            req.headers = {};
+            req.headers['x-github-event'] = 'err';
+            req.headers['x-github-delivery'] = '123';
+
+            await handler(req, response);
+
+            sinon.assert.calledOnce(configStub);
+            sinon.assert.notCalled(spy);
+            sinon.assert.notCalled(sendStatusStub);
+            sinon.assert.called(sendStub);
+        });
+    });
+
+    describe("loadProbot", () => {
+        let bootstrapper: GCFBootstrapper;
+        let configStub: sinon.SinonStub<[], Promise<Options>>;
+
+
+        beforeEach(() => {
+            bootstrapper = new GCFBootstrapper();
+            configStub = sinon.stub(bootstrapper, "getProbotConfig").callsFake(() => {
+                return Promise.resolve({ id: 1234, secret: "foo", webhookPath: "bar" })
+            });
+        });
+
+        afterEach(() => {
+            configStub.reset();
+        })
+
+        it("gets the config", async () => {
+            await bootstrapper.loadProbot(async app => {
+                // Do nothing
+            });
+            sinon.assert.calledOnce(configStub);
+        });
+
+        it("caches the probot if initialized", async () => {
+            await bootstrapper.loadProbot(async app => {
+                // Do nothing
+            });
+            sinon.assert.calledOnce(configStub);
+            await bootstrapper.loadProbot(async app => {
+                // Do nothing again
+            });
+            sinon.assert.calledOnce(configStub);
+        });
+    });
+});
