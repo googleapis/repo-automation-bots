d3ddaf268a2a4e9ca7676d71dcabec6b7dcf4229
Colin Nelson <orthros@users.noreply.github.com>
2019-08-02 11:35:41 -0700

    feat(gcf-utils): utility to add/upload probot secrets. (#18)

---
+++ packages/gcf-utils/src/bin/genkey.ts
@@ -0,0 +1,151 @@
+#!/usr/bin/env node
+/**
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import fs from 'fs';
+import path from 'path';
+import * as yargs from 'yargs';
+import { Argv } from 'yargs';
+import * as KMS from '@google-cloud/kms';
+import { Storage, StorageOptions } from '@google-cloud/storage';
+import { Base64 } from 'js-base64';
+import { Options } from 'probot';
+import * as tmp from 'tmp';
+
+let argv = yargs
+    .command('gen', "Create and upload a client key.", (yargs: Argv) => {
+        return yargs.option('keyfile', {
+            describe: "The path to the .pem keyfile",
+            type: 'string',
+            default: "key.pem",
+        }).option('verbose', {
+            alias: 'v',
+            default: false,
+            type: 'boolean'
+        }).option('project', {
+            describe: 'Name of GCP project',
+            alias: 'p',
+            type: 'string',
+            demand: true
+        }).option('location', {
+            alias: 'l',
+            desription: 'Keyring location',
+            type: 'string',
+            default: 'global'
+        }).option('bot', {
+            alias: 'b',
+            type: 'string',
+            description: 'Name of the bot'
+        }).option('bucket', {
+            alias: 'bu',
+            type: 'string',
+            description: 'Name of the Bucket'
+        }).option('id', {
+            alias: 'i',
+            type: 'string',
+            description: 'ID of the GitHub Application'
+        });
+    }).argv;
+
+
+const keyfile: string = argv.file as string || 'key.pem';
+const project: string = argv.project as string;
+const location: string = argv.location as string || 'global';
+const keyring: string = argv.keyring as string || 'probot-keys';
+const bucketName: string = argv.bucket as string;
+const botname: string = argv.bot as string;
+const webhookSecret: string = argv.secret as string;
+const id: number = argv.id as number;
+
+
+if (!project) {
+    console.error('Project name is required');
+    yargs.showHelp();
+    process.exit(1);
+}
+if (!botname) {
+    console.error('Name of the bot is required');
+    yargs.showHelp();
+    process.exit(1);
+}
+if (!webhookSecret) {
+    console.error('Webhook secret is required');
+    yargs.showHelp();
+    process.exit(1);
+}
+if (!id) {
+    console.error('GitHub Application ID is required');
+    yargs.showHelp();
+    process.exit(1);
+}
+if (!bucketName) {
+    console.error('Bucket Name is required');
+    yargs.showHelp();
+    process.exit(1);
+}
+
+let keyContent: string = '';
+try {
+    keyContent = fs.readFileSync(keyfile, 'utf8');
+} catch (e) {
+    console.log(`Error reading file: ${keyfile}`);
+    process.exit(1);
+}
+
+let blob: Options = {
+    cert: keyContent,
+    id: id,
+    secret: webhookSecret
+}
+
+async function run() {
+    let encblob: Buffer = Buffer.from('');
+    try {
+        const opts = project ? { projectId: project } as KMS.v1.KeyManagementServiceClient.ConfigurationObject : undefined;
+
+        const kmsclient = new KMS.KeyManagementServiceClient(opts);
+
+        const name = kmsclient.cryptoKeyPath(
+            project,
+            location,
+            keyring,
+            botname
+        );
+
+        let plaintext = Base64.encode(JSON.stringify(blob));
+        const [kmsresult] = await kmsclient.encrypt({ name, plaintext: plaintext });
+        encblob = kmsresult.ciphertext;
+
+    } catch (e) {
+        console.error('Got an error encrypting the blob');
+        console.error(e);
+        process.exit(1);
+    }
+
+    const options = (project) ? { project } as StorageOptions : undefined;
+    const storage = new Storage(options);
+
+    let tmpobj = tmp.dirSync();
+    console.log('Dir: ', tmpobj.name);
+
+    const fileName = path.join(tmpobj.name, botname);
+
+    fs.writeFileSync(fileName, encblob);
+
+    await storage.bucket(bucketName).upload(fileName);
+    tmpobj.removeCallback();
+}
+run();
