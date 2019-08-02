5ab850cfa02825ead27539bbd0b732af9267698b
Jeff Ching <chingor@google.com>
2019-08-02 11:28:13 -0700

    feat: header-checker-lint bot framework
    Implement naive license header detection/checks
    
    Clean out CHANGELOG and set package.json to 1.0.0
    
    Add tests

---
+++ packages/header-checker-lint/src/index.d.ts
@@ -0,0 +1,6 @@
+// define types for a few modules used by probot that do not have their
+// own definitions published. Before taking this step, folks should first
+// check whether type bindings are already published.
+declare module "promise-events" {
+  class EventEmitter {}
+}
---
+++ packages/header-checker-lint/test/header-checker-lint.ts
@@ -0,0 +1,183 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import myProbotApp from '../src/header-checker-lint';
+
+import { resolve } from 'path';
+import { Probot } from 'probot';
+import snapshot from 'snap-shot-it';
+
+import nock from 'nock';
+nock.disableNetConnect();
+
+const fixturesPath = resolve(__dirname, '../../test/fixtures');
+
+// TODO: stop disabling warn once the following upstream patch is landed:
+// https://github.com/probot/probot/pull/926
+global.console.warn = () => {};
+
+describe('HeaderCheckerLint', () => {
+  let probot: Probot;
+
+  beforeEach(() => {
+    probot = new Probot({
+      // use a bare instance of octokit, the default version
+      // enables retries which makes testing difficult.
+      Octokit: require('@octokit/rest'),
+    });
+
+    const app = probot.load(myProbotApp);
+    app.app = {
+      getSignedJsonWebToken() {
+        return 'abc123';
+      },
+      getInstallationAccessToken(): Promise<string> {
+        return Promise.resolve('abc123');
+      },
+    };
+  });
+
+  describe('opened pull request', () => {
+    let payload : any;
+
+    beforeEach(() => {
+      payload = require(resolve(
+        fixturesPath,
+        './pull_request_opened'
+      ));
+    });
+
+    it('sets a "failure" context on PR, if new source file is missing license', async () => {
+      const invalidFiles = require(resolve(fixturesPath, './missing_license_added'));
+      const blob = require(resolve(fixturesPath, './missing_license'));
+      const requests = nock('https://api.github.com')
+        .get('/repos/chingor13/google-auth-library-java/pulls/3/files?per_page=100')
+        .reply(200, invalidFiles)
+        .get('/repos/chingor13/google-auth-library-java/git/blobs/5b414a072e40622c177c72a58efb74ff9faadd0d')
+        .reply(200, blob)
+        .post('/repos/chingor13/google-auth-library-java/check-runs', body => {
+          snapshot(body);
+          return true;
+        })
+        .reply(200);
+
+      await probot.receive({ name: 'pull_request', payload: payload, id: 'abc123' });
+      requests.done();
+    });
+
+    it('sets a "failure" context on PR, if a modified source file is missing license', async () => {
+      const invalidFiles = require(resolve(fixturesPath, './missing_license_modified'));
+      const blob = require(resolve(fixturesPath, './missing_license'));
+      const requests = nock('https://api.github.com')
+        .get('/repos/chingor13/google-auth-library-java/pulls/3/files?per_page=100')
+        .reply(200, invalidFiles)
+        .get('/repos/chingor13/google-auth-library-java/git/blobs/5b414a072e40622c177c72a58efb74ff9faadd0d')
+        .reply(200, blob)
+        .post('/repos/chingor13/google-auth-library-java/check-runs', body => {
+          snapshot(body);
+          return true;
+        })
+        .reply(200);
+
+      await probot.receive({ name: 'pull_request', payload: payload, id: 'abc123' });
+      requests.done();
+    });
+
+    it('sets a "failure" context on PR, if the new source file is added and has wrong year', async () => {
+      const invalidFiles = require(resolve(fixturesPath, './wrong_year_added'));
+      const blob = require(resolve(fixturesPath, './wrong_year'));
+      const requests = nock('https://api.github.com')
+        .get('/repos/chingor13/google-auth-library-java/pulls/3/files?per_page=100')
+        .reply(200, invalidFiles)
+        .get('/repos/chingor13/google-auth-library-java/git/blobs/ef039bb72b6cadc9c144541a5645e4a6818fb6de')
+        .reply(200, blob)
+        .post('/repos/chingor13/google-auth-library-java/check-runs', body => {
+          snapshot(body);
+          return true;
+        })
+        .reply(200);
+
+      await probot.receive({ name: 'pull_request', payload: payload, id: 'abc123' });
+      requests.done();
+    });
+
+  });
+
+  describe('updated pull request', () => {
+    let payload : any;
+
+    before(() => {
+      payload = require(resolve(
+        fixturesPath,
+        './pull_request_synchronized'
+      ));
+    });
+
+    it('sets a "failure" context on PR, if new source file is missing license', async () => {
+      const invalidFiles = require(resolve(fixturesPath, './missing_license_added'));
+      const blob = require(resolve(fixturesPath, './missing_license'));
+      const requests = nock('https://api.github.com')
+        .get('/repos/chingor13/google-auth-library-java/pulls/3/files?per_page=100')
+        .reply(200, invalidFiles)
+        .get('/repos/chingor13/google-auth-library-java/git/blobs/5b414a072e40622c177c72a58efb74ff9faadd0d')
+        .reply(200, blob)
+        .post('/repos/chingor13/google-auth-library-java/check-runs', body => {
+          snapshot(body);
+          return true;
+        })
+        .reply(200);
+
+      await probot.receive({ name: 'pull_request', payload: payload, id: 'abc123' });
+      requests.done();
+    });
+
+    it('sets a "failure" context on PR, if a modified source file is missing license', async () => {
+      const invalidFiles = require(resolve(fixturesPath, './missing_license_modified'));
+      const blob = require(resolve(fixturesPath, './missing_license'));
+      const requests = nock('https://api.github.com')
+        .get('/repos/chingor13/google-auth-library-java/pulls/3/files?per_page=100')
+        .reply(200, invalidFiles)
+        .get('/repos/chingor13/google-auth-library-java/git/blobs/5b414a072e40622c177c72a58efb74ff9faadd0d')
+        .reply(200, blob)
+        .post('/repos/chingor13/google-auth-library-java/check-runs', body => {
+          snapshot(body);
+          return true;
+        })
+        .reply(200);
+
+      await probot.receive({ name: 'pull_request', payload: payload, id: 'abc123' });
+      requests.done();
+    });
+
+    it('sets a "failure" context on PR, if the new source file is added and has wrong year', async () => {
+      const invalidFiles = require(resolve(fixturesPath, './wrong_year_added'));
+      const blob = require(resolve(fixturesPath, './wrong_year'));
+      const requests = nock('https://api.github.com')
+        .get('/repos/chingor13/google-auth-library-java/pulls/3/files?per_page=100')
+        .reply(200, invalidFiles)
+        .get('/repos/chingor13/google-auth-library-java/git/blobs/ef039bb72b6cadc9c144541a5645e4a6818fb6de')
+        .reply(200, blob)
+        .post('/repos/chingor13/google-auth-library-java/check-runs', body => {
+          snapshot(body);
+          return true;
+        })
+        .reply(200);
+
+      await probot.receive({ name: 'pull_request', payload: payload, id: 'abc123' });
+      requests.done();
+    });
+  });
+});
