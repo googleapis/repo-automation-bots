36654186af895e96e2d240e6fd99eb9b476f287d
Justin Beckwith <justin.beckwith@gmail.com>
2019-08-10 18:17:31 -0700

    feat: add label-sync bot

---
+++ packages/label-sync/test/test.ts
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import appFn from '../src/label-sync';
+import { assert } from 'chai';
+
+import nock from 'nock';
+import { Probot } from 'probot';
+nock.disableNetConnect();
+
+describe('Label Sync', () => {
+  let probot: Probot;
+
+  beforeEach(() => {
+    probot = new Probot({
+      // use a bare instance of octokit, the default version
+      // enables retries which makes testing difficult.
+      Octokit: require('@octokit/rest'),
+    });
+
+    const app = probot.load(appFn);
+    app.app = {
+      getSignedJsonWebToken() {
+        return 'abc123';
+      },
+      getInstallationAccessToken(): Promise<string> {
+        return Promise.resolve('abc123');
+      },
+    };
+  });
+
+  it('should have tests', () => {
+    assert.isTrue(true);
+  });
+});
