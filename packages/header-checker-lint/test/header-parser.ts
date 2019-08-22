0844334d75cc4d152fd09a0ca6b64e754f25c455
Jeff Ching <chingor@google.com>
2019-08-22 13:47:15 -0700

    test: add header-parser tests

---
+++ packages/header-checker-lint/test/header-parser.ts
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { detectLicenseHeader } from '../src/header-parser';
+
+import { resolve } from 'path';
+import fs from 'fs';
+import assert from 'assert';
+
+const fixturesPath = resolve(__dirname, '../../test/fixtures');
+
+describe('detectLicenseHeader', () => {
+  it('should handle c-style comments', async () => {
+    const contents = fs.readFileSync(
+      resolve(fixturesPath, './c-style-header.txt'),
+      'utf-8'
+    );
+    const header = detectLicenseHeader(contents);
+    assert.strictEqual(header.copyright, 'Google LLC');
+    assert.strictEqual(header.year, 2019);
+    assert.strictEqual(header.type, 'Apache-2.0');
+  });
+
+  it('should handle c-style extended comments', async () => {
+    const contents = fs.readFileSync(
+      resolve(fixturesPath, './c-style-header-extended.txt'),
+      'utf-8'
+    );
+    const header = detectLicenseHeader(contents);
+    assert.strictEqual(header.copyright, 'Google LLC. All Rights Reserved.');
+    assert.strictEqual(header.year, 2019);
+    assert.strictEqual(header.type, 'Apache-2.0');
+  });
+
+  it('should handle bash-style comments', async () => {
+    const contents = fs.readFileSync(
+      resolve(fixturesPath, './bash-style-header.txt'),
+      'utf-8'
+    );
+    const header = detectLicenseHeader(contents);
+    assert.strictEqual(header.copyright, 'Google LLC');
+    assert.strictEqual(header.year, 2019);
+    assert.strictEqual(header.type, 'Apache-2.0');
+  });
+});
