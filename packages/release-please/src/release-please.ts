6323f532379d2c6503cc9bf2e48bf8fe1f9d47ed
Jeff Ching <chingor@google.com>
2019-09-09 16:09:45 -0700

    feat: add release-please bot (#29)
    * feat: add release-please bot
    
    * feat: use release-please to create a PR
    
    * chore: fix missing license header on index.d.ts
    
    * fix: inject authenticated octokit instance
    
    * fix: use master
    
    * feat: read configuration from well-known location
    
    * refactor: cleanup variable names
    
    * fix: use .github folder for well-known location
    
    * feat: support additional languages via release-please factory
    
    * chore: fix package name
    
    * fix: use built-in probot yaml config loading
    
    * docs: add configuration options to README
    
    * test: add some tests
    
    * test: adding non-master push test
    
    * test: cleanup test assertions
    
    * test: add test for overriding release type
    
    * test: add test for overwriting labels
    
    * fix: lint
    
    * docs: add README link to release-please bot
    
    * fix: use toLowerCase()

---
+++ packages/release-please/src/release-please.ts
@@ -0,0 +1,99 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Application } from 'probot';
+
+// TODO: fix these imports when release-please exports types from the root
+// See https://github.com/googleapis/release-please/issues/249
+import { ReleaseType, BuildOptions } from 'release-please/build/src/release-pr';
+import { ReleasePRFactory } from 'release-please/build/src/release-pr-factory';
+import { Runner } from './runner';
+
+interface ConfigurationOptions {
+  primaryBranch: string;
+  releaseLabels?: string[];
+  releaseType?: ReleaseType;
+}
+
+const DEFAULT_API_URL = 'https://api.github.com';
+const WELL_KNOWN_CONFIGURATION_FILE = 'release-please.yml';
+const DEFAULT_CONFIGURATION: ConfigurationOptions = {
+  primaryBranch: 'master',
+  releaseLabels: ['autorelease: pending', 'type: process'],
+};
+
+function releaseTypeFromRepoLanguage(language: string | null): ReleaseType {
+  if (language == null) {
+    throw Error('repository has no detected language');
+  }
+  switch (language.toLowerCase()) {
+    case 'ruby':
+      return ReleaseType.RubyYoshi;
+    case 'java':
+      return ReleaseType.JavaYoshi;
+    case 'typescript':
+    case 'javascript':
+      return ReleaseType.Node;
+    case 'php':
+      return ReleaseType.PHPYoshi;
+    default:
+      throw Error(`unknown release type: ${language}`);
+  }
+}
+
+export = (app: Application) => {
+  app.on('push', async context => {
+    const repoUrl = context.payload.repository.full_name;
+    const branch = context.payload.ref.replace('refs/heads/', '');
+    const repoName = context.payload.repository.name;
+
+    const configuration = (await context.config(
+      WELL_KNOWN_CONFIGURATION_FILE,
+      DEFAULT_CONFIGURATION,
+      {
+        arrayMerge: (defaultArray, sourceArray, _options) => {
+          return sourceArray ? sourceArray : defaultArray;
+        },
+      }
+    )) as ConfigurationOptions;
+
+    if (branch !== configuration.primaryBranch) {
+      app.log.info(
+        `Not on primary branch (${configuration.primaryBranch}): ${branch}`
+      );
+      return;
+    }
+
+    const releaseType = configuration.releaseType
+      ? configuration.releaseType
+      : releaseTypeFromRepoLanguage(context.payload.repository.language);
+    const buildOptions: BuildOptions = {
+      packageName: repoName,
+      repoUrl,
+      apiUrl: DEFAULT_API_URL,
+      octokitAPIs: {
+        octokit: context.github,
+        graphql: context.github.graphql,
+        request: context.github.request,
+      },
+    };
+    if (configuration.releaseLabels) {
+      buildOptions.label = configuration.releaseLabels.join(',');
+    }
+
+    Runner.runner(ReleasePRFactory.build(releaseType, buildOptions));
+  });
+};
