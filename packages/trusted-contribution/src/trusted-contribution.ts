ef09b573f2d1c621d1d0130c1147b328ecb5c043
Christopher Wilcox <crwilcox@google.com>
2019-10-14 16:43:57 -0700

    fix(trusted contribution): Limit the types of pull request events responded to.

--- packages/trusted-contribution/src/trusted-contribution.ts
+++ packages/trusted-contribution/src/trusted-contribution.ts
@@ -38,7 +38,7 @@ function isTrustedContribution(author: string): boolean {
 }
 
 export = (app: Application) => {
-  app.on('pull_request', async context => {
+  app.on(['pull_request.opened', 'pull_request.edited'], async context => {
     const PR_AUTHOR = context.payload.pull_request.user.login;
 
     // TODO: add additional verification that only dependency version changes occurred.
