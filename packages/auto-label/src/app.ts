bd23db02e8f21ce01df0fb06155082659706e3c0
Sofia Leon <sofialeon@google.com>
2020-01-28 13:58:51 -0800

    fix rebase

--- packages/auto-label/src/app.ts
+++ packages/auto-label/src/app.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 // Copyright 2020 Google LLC
-=======
-// Copyright 2019 Google LLC
->>>>>>> d03795813fa2793da69d5429c734f08fb94e3e24
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
