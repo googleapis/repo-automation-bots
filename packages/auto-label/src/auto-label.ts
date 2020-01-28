eeeee5402afa8e995d386b86414315fdcbe086eb
Sofia Leon <sofialeon@google.com>
2020-01-27 18:08:57 -0800

    saving auto-label, most tests complete

--- packages/auto-label/src/auto-label.ts
+++ packages/auto-label/src/auto-label.ts
@@ -16,20 +16,74 @@
 import { Application} from 'probot';
 import { GitHubAPI } from 'probot/lib/github';
 import * as path from 'path';
-import { POINT_CONVERSION_COMPRESSED } from 'constants';
-//import * as fs from 'fs';
+import e = require('express');
+
 const fs = require('fs');
-const jsonData = path.resolve(__dirname, '../../src/downloadedfile.txt');
+//const jsonData = path.resolve(__dirname, '../../src/downloadedfile.txt');
 const colorsData = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../src/colors.json'), 'utf8'));
 
-async function callStorage() {
+handler.addLabels = async function addLabels(github: GitHubAPI, owner: string, repo: string, issueNumber: number, labels: string[]) {
+  try {
+      const data = await github.issues.addLabels({
+          owner: owner,
+          repo: repo,
+          issue_number: issueNumber,
+          labels: labels
+      })
+      console.log(data);
+      return data;
+  } catch(err) {
+      return null;
+  }
+}
 
+handler.checkExistingLabels = async function checkExistingLabels(github: GitHubAPI, owner: string, repo: string, name: string) {
+  try {
+      const data = await github.issues.getLabel({
+          owner: owner,
+          repo: repo,
+          name: name
+      })
+      console.log("checkExistingLabels "+data.data.name);
+      return data.data.name;
+  } catch(err) {
+      return null;
+  }
+}
 
+handler.createLabel = async function createLabel(github: GitHubAPI, owner: string, repo: string, name: string, color: string) {
+  try {
+      const data = await github.issues.createLabel({
+          owner: owner,
+          repo: repo,
+          name: name,
+          color: color
+      })
+      return data;
+  } catch(err) {
+      return null;
+  }
+}
+
+handler.checkExistingIssueLabels = async function checkExistingIssueLabels(github: GitHubAPI, owner: string, repo: string, issue_number: number) {
+  try {
+      const data = await github.issues.listLabelsOnIssue({
+          owner: owner,
+          repo: repo,
+          issue_number: issue_number
+      });
+      console.log("checkLabelsOnIssue ")
+      data.data.forEach(element => console.log(element));
+      return data.data;
+  } catch(err) {
+      return null;
+  }
+}
+
+handler.callStorage = async function callStorage(bucketName: any, srcFileName: any, destFileName: string) {
   const {Storage} = require('@google-cloud/storage');
   const storage = new Storage();
-  const bucketName = 'devrel-dev-settings';
-  const srcFilename = 'public_repos.json';
-  const destFilename = path.resolve("src/downloadedfile.txt")
+  let destFilename = path.resolve(destFileName);
 
   const options = {
   // The path to which the file should be downloaded, e.g. "./file.txt"
@@ -39,94 +93,84 @@ async function callStorage() {
   // Downloads the file
   await storage
   .bucket(bucketName)
-  .file(srcFilename)
+  .file(srcFileName)
   .download(options);
 
   console.log(
-  `gs://${bucketName}/${srcFilename} downloaded to ${destFilename}.`
+  `gs://${bucketName}/${srcFileName} downloaded to ${destFilename}.`
   );
-}
-
-async function createLabel(github: GitHubAPI, owner: string, repo: string, name: string, color: string) {
-  try {
-      const data = await github.issues.createLabel({
-          owner: owner,
-          repo: repo,
-          name: name,
-          color: color
-      })
-      console.log(await data);
-      console.log('a');
-      return data;
-  } catch(err) {
-    console.log('b');
-      return null;
-  }
-}
-
-async function addLabels(github: GitHubAPI, owner: string, repo: string, issueNumber: number, labels: string[]) {
-  try {
-      const data = await github.issues.addLabels({
-          owner: owner,
-          repo: repo,
-          issue_number: issueNumber,
-          labels: labels
-      })
-      console.log('d');
-      console.log(await data);
-      return data;
-  } catch(err) {
-    console.log('e');
-      return null;
-  }
+  return path.resolve(__dirname, `../../${destFileName}`);
 }
 
 
-export = (app: Application) => {
-  
 
+function handler(app: Application) {
   app.on(
     [
-      'issues.opened'
+      'issues.opened',
+      'issues.reopened'
     ],
     async context => {
-      
       const owner = context.payload.repository.owner.login;
-      console.log(owner);
-      const repoName = 'sofisl/'+context.payload.repository.name;
-      console.log(repoName);
       const repo = context.payload.repository.name;
-      console.log(repo);
-      const issueId = context.payload.issue.id;
+      const issueId = context.payload.issue.number;
       console.log(issueId);
       let flag = false;
 
       //get repo mapping from cloud Storage
-      //await callStorage();
+      
+      let jsonData = await handler.callStorage('devrel-dev-settings', 'public_repos.json','src/downloadedfile.txt');
+      const jsonArray = JSON.parse(fs.readFileSync(jsonData)).repos;
 
-      const jsonArray = JSON.parse(fs.readFileSync(jsonData, 'utf8')).repos;
-
-      if (jsonArray.length != 0) {
-        jsonArray.forEach((element: any) => {
-          let objectValues = Object.values(element);
-          let elementNumber = objectValues.indexOf(repoName);
-          let colorNumber = jsonArray.findIndex(function(object: any) { return element = object});
-          if (elementNumber != -1) {
-            const githubLabel = element.github_label;
-            console.log(githubLabel);
-            createLabel(context.github, 'sofisl', 'mergeOnGreenTest', githubLabel, colorsData[colorNumber].color);
-            addLabels(context.github, 'sofisl', repo, issueId, githubLabel);
-            flag = true;
-            return;
-          }
-        })
+      //const jsonArray = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../src/downloadedfile.txt'))).repos;
+     
+      if (jsonArray.length == 0) {
+        console.log("JSON file downloaded from Cloud Storage was empty")
+        return;
+      }
+      console.log(owner+"/"+repo);
+      let objectInJsonArray = jsonArray.find((element: { repo: string; }) => element.repo === (owner+"/"+repo));
+      console.log(objectInJsonArray);
+      if(objectInJsonArray == undefined) {
+        console.log("There was no match for the repo name: "+repo)
+        return;
       }
 
-       if (!flag) {
-        console.log("There was no match");
-       }
+      let colorNumber = jsonArray.findIndex((object: any) =>  objectInJsonArray === object);
+      let githubLabel = objectInJsonArray.github_label
+      let alreadyExists = await handler.checkExistingLabels(context.github, owner, repo, githubLabel);  
 
-       return;
+      if (alreadyExists == undefined || alreadyExists == null) {
+        handler.createLabel(context.github, owner, repo, githubLabel, colorsData[colorNumber].color);
+      } else {
+        console.log("This label already exists on the repository, will check if it also exists on the issue");
+      }
+      let labelsOnIssue = await handler.checkExistingIssueLabels(context.github, owner, repo, issueId);
+      if (labelsOnIssue != undefined && labelsOnIssue != null) {
+        let found = labelsOnIssue.find((element: { name: string; }) => element.name === githubLabel);
+        if (found == undefined || found == null) {
+          handler.addLabels(context.github, owner, repo, issueId, [`${objectInJsonArray.github_label}`]);
+          flag = true;
+        } else {
+         console.log("This label already exists on this issue")
+         return;
+        }
+      } else {
+        await handler.addLabels(context.github, owner, repo, issueId, [`${objectInJsonArray.github_label}`]);
+        flag = true;
+      }
      
+
+       if (!flag) {
+        console.log("There was no match for repo name: "+repo);
+       }
+       
+       return;
+
     })
+
 };
+
+
+export = handler;
+
