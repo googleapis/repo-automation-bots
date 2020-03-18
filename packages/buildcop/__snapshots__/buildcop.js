daf18f809cfe32411953022b90d8516d2159875c
Tyler Bui-Palsulich <26876514+tbpg@users.noreply.github.com>
2020-03-18 11:08:26 -0400

    chore(Build Cop): rename build ID to commit (#398)
    Once this is fully deployed and we're sure no more builds will send the
    buildID field, we can delete it.

--- packages/buildcop/__snapshots__/buildcop.js
+++ packages/buildcop/__snapshots__/buildcop.js
@@ -1,6 +1,6 @@
 exports['buildcop app testsFailed opens an issue when testsFailed 1'] = {
   "title": "The build failed",
-  "body": "This test failed!\n\nTo configure my behavior, see [the Build Cop Bot documentation](https://github.com/googleapis/repo-automation-bots/tree/master/packages/buildcop).\n\nIf I'm commenting on this issue too often, add the `buildcop: quiet` label and\nI will stop commenting.\n\n---\n\nbuildID: 123\nbuildURL: http://example.com\nstatus: failed",
+  "body": "This test failed!\n\nTo configure my behavior, see [the Build Cop Bot documentation](https://github.com/googleapis/repo-automation-bots/tree/master/packages/buildcop).\n\nIf I'm commenting on this issue too often, add the `buildcop: quiet` label and\nI will stop commenting.\n\n---\n\ncommit: 123\nbuildURL: http://example.com\nstatus: failed",
   "labels": [
     "type: bug",
     "priority: p1",
@@ -10,7 +10,7 @@ exports['buildcop app testsFailed opens an issue when testsFailed 1'] = {
 
 exports['buildcop app testsFailed opens a new issue when testsFailed and there is a previous one closed 1'] = {
   "title": "The build failed",
-  "body": "This test failed!\n\nTo configure my behavior, see [the Build Cop Bot documentation](https://github.com/googleapis/repo-automation-bots/tree/master/packages/buildcop).\n\nIf I'm commenting on this issue too often, add the `buildcop: quiet` label and\nI will stop commenting.\n\n---\n\nbuildID: 123\nbuildURL: http://example.com\nstatus: failed",
+  "body": "This test failed!\n\nTo configure my behavior, see [the Build Cop Bot documentation](https://github.com/googleapis/repo-automation-bots/tree/master/packages/buildcop).\n\nIf I'm commenting on this issue too often, add the `buildcop: quiet` label and\nI will stop commenting.\n\n---\n\ncommit: 123\nbuildURL: http://example.com\nstatus: failed",
   "labels": [
     "type: bug",
     "priority: p1",
@@ -19,12 +19,12 @@ exports['buildcop app testsFailed opens a new issue when testsFailed and there i
 }
 
 exports['buildcop app testsFailed comments on an existing open issue when testsFailed 1'] = {
-  "body": "buildID: 123\nbuildURL: http://example.com\nstatus: failed"
+  "body": "commit: 123\nbuildURL: http://example.com\nstatus: failed"
 }
 
 exports['buildcop app xunitXML opens an issue [Go] 1'] = {
   "title": "spanner/spanner_snippets: TestSample failed",
-  "body": "This test failed!\n\nTo configure my behavior, see [the Build Cop Bot documentation](https://github.com/googleapis/repo-automation-bots/tree/master/packages/buildcop).\n\nIf I'm commenting on this issue too often, add the `buildcop: quiet` label and\nI will stop commenting.\n\n---\n\nbuildID: 123\nbuildURL: http://example.com\nstatus: failed",
+  "body": "This test failed!\n\nTo configure my behavior, see [the Build Cop Bot documentation](https://github.com/googleapis/repo-automation-bots/tree/master/packages/buildcop).\n\nIf I'm commenting on this issue too often, add the `buildcop: quiet` label and\nI will stop commenting.\n\n---\n\ncommit: 123\nbuildURL: http://example.com\nstatus: failed",
   "labels": [
     "type: bug",
     "priority: p1",
@@ -42,7 +42,7 @@ exports['buildcop app xunitXML closes a duplicate issue 2'] = {
 
 exports['buildcop app xunitXML opens an issue [Python] 1'] = {
   "title": "appengine.flexible.datastore.main_test: test_index failed",
-  "body": "This test failed!\n\nTo configure my behavior, see [the Build Cop Bot documentation](https://github.com/googleapis/repo-automation-bots/tree/master/packages/buildcop).\n\nIf I'm commenting on this issue too often, add the `buildcop: quiet` label and\nI will stop commenting.\n\n---\n\nbuildID: 123\nbuildURL: http://example.com\nstatus: failed",
+  "body": "This test failed!\n\nTo configure my behavior, see [the Build Cop Bot documentation](https://github.com/googleapis/repo-automation-bots/tree/master/packages/buildcop).\n\nIf I'm commenting on this issue too often, add the `buildcop: quiet` label and\nI will stop commenting.\n\n---\n\ncommit: 123\nbuildURL: http://example.com\nstatus: failed",
   "labels": [
     "type: bug",
     "priority: p1",
@@ -51,7 +51,7 @@ exports['buildcop app xunitXML opens an issue [Python] 1'] = {
 }
 
 exports['buildcop app xunitXML comments on existing issue 1'] = {
-  "body": "buildID: 123\nbuildURL: http://example.com\nstatus: failed"
+  "body": "commit: 123\nbuildURL: http://example.com\nstatus: failed"
 }
 
 exports['buildcop app xunitXML reopens issue for failing test 1'] = {
@@ -66,11 +66,11 @@ exports['buildcop app xunitXML reopens issue for failing test 1'] = {
 }
 
 exports['buildcop app xunitXML reopens issue for failing test 2'] = {
-  "body": "Oops! Looks like this issue is still flaky. :grimacing:\n\nI reopened the issue, but a human will need to close it again.\n\n---\n\nbuildID: 123\nbuildURL: http://example.com\nstatus: failed"
+  "body": "Oops! Looks like this issue is still flaky. :grimacing:\n\nI reopened the issue, but a human will need to close it again.\n\n---\n\ncommit: 123\nbuildURL: http://example.com\nstatus: failed"
 }
 
 exports['buildcop app xunitXML closes an issue for a passing test [Go] 1'] = {
-  "body": "Test passed in build 123 (http://example.com)! Closing this issue."
+  "body": "Test passed for commit 123 (http://example.com)! Closing this issue."
 }
 
 exports['buildcop app xunitXML closes an issue for a passing test [Go] 2'] = {
@@ -78,7 +78,7 @@ exports['buildcop app xunitXML closes an issue for a passing test [Go] 2'] = {
 }
 
 exports['buildcop app xunitXML closes an issue for a passing test [Python] 1'] = {
-  "body": "Test passed in build 123 (http://example.com)! Closing this issue."
+  "body": "Test passed for commit 123 (http://example.com)! Closing this issue."
 }
 
 exports['buildcop app xunitXML closes an issue for a passing test [Python] 2'] = {
@@ -87,7 +87,7 @@ exports['buildcop app xunitXML closes an issue for a passing test [Python] 2'] =
 
 exports['buildcop app xunitXML opens multiple issues for multiple failures 1'] = {
   "title": "storage/buckets: TestBucketLock failed",
-  "body": "This test failed!\n\nTo configure my behavior, see [the Build Cop Bot documentation](https://github.com/googleapis/repo-automation-bots/tree/master/packages/buildcop).\n\nIf I'm commenting on this issue too often, add the `buildcop: quiet` label and\nI will stop commenting.\n\n---\n\nbuildID: 123\nbuildURL: http://example.com\nstatus: failed",
+  "body": "This test failed!\n\nTo configure my behavior, see [the Build Cop Bot documentation](https://github.com/googleapis/repo-automation-bots/tree/master/packages/buildcop).\n\nIf I'm commenting on this issue too often, add the `buildcop: quiet` label and\nI will stop commenting.\n\n---\n\ncommit: 123\nbuildURL: http://example.com\nstatus: failed",
   "labels": [
     "type: bug",
     "priority: p1",
@@ -97,7 +97,7 @@ exports['buildcop app xunitXML opens multiple issues for multiple failures 1'] =
 
 exports['buildcop app xunitXML opens multiple issues for multiple failures 2'] = {
   "title": "storage/buckets: TestUniformBucketLevelAccess failed",
-  "body": "This test failed!\n\nTo configure my behavior, see [the Build Cop Bot documentation](https://github.com/googleapis/repo-automation-bots/tree/master/packages/buildcop).\n\nIf I'm commenting on this issue too often, add the `buildcop: quiet` label and\nI will stop commenting.\n\n---\n\nbuildID: 123\nbuildURL: http://example.com\nstatus: failed",
+  "body": "This test failed!\n\nTo configure my behavior, see [the Build Cop Bot documentation](https://github.com/googleapis/repo-automation-bots/tree/master/packages/buildcop).\n\nIf I'm commenting on this issue too often, add the `buildcop: quiet` label and\nI will stop commenting.\n\n---\n\ncommit: 123\nbuildURL: http://example.com\nstatus: failed",
   "labels": [
     "type: bug",
     "priority: p1",
@@ -107,7 +107,7 @@ exports['buildcop app xunitXML opens multiple issues for multiple failures 2'] =
 
 exports['buildcop app xunitXML opens an issue [Java] 1'] = {
   "title": "com.google.cloud.vision.it.ITSystemTest: detectSafeSearchGcsTest failed",
-  "body": "This test failed!\n\nTo configure my behavior, see [the Build Cop Bot documentation](https://github.com/googleapis/repo-automation-bots/tree/master/packages/buildcop).\n\nIf I'm commenting on this issue too often, add the `buildcop: quiet` label and\nI will stop commenting.\n\n---\n\nbuildID: 123\nbuildURL: http://example.com\nstatus: failed",
+  "body": "This test failed!\n\nTo configure my behavior, see [the Build Cop Bot documentation](https://github.com/googleapis/repo-automation-bots/tree/master/packages/buildcop).\n\nIf I'm commenting on this issue too often, add the `buildcop: quiet` label and\nI will stop commenting.\n\n---\n\ncommit: 123\nbuildURL: http://example.com\nstatus: failed",
   "labels": [
     "type: bug",
     "priority: p1",
@@ -116,7 +116,7 @@ exports['buildcop app xunitXML opens an issue [Java] 1'] = {
 }
 
 exports['buildcop app xunitXML closes an issue for a passing test [Java] 1'] = {
-  "body": "Test passed in build 123 (http://example.com)! Closing this issue."
+  "body": "Test passed for commit 123 (http://example.com)! Closing this issue."
 }
 
 exports['buildcop app xunitXML closes an issue for a passing test [Java] 2'] = {
@@ -134,11 +134,11 @@ exports['buildcop app reopens the original flaky issue when there is a duplicate
 }
 
 exports['buildcop app reopens the original flaky issue when there is a duplicate 2'] = {
-  "body": "Oops! Looks like this issue is still flaky. :grimacing:\n\nI reopened the issue, but a human will need to close it again.\n\n---\n\nbuildID: 123\nbuildURL: http://example.com\nstatus: failed"
+  "body": "Oops! Looks like this issue is still flaky. :grimacing:\n\nI reopened the issue, but a human will need to close it again.\n\n---\n\ncommit: 123\nbuildURL: http://example.com\nstatus: failed"
 }
 
 exports['buildcop app xunitXML does not comment about failure on existing flaky issue 1'] = {
-  "body": "buildID: 123\nbuildURL: http://example.com\nstatus: failed"
+  "body": "commit: 123\nbuildURL: http://example.com\nstatus: failed"
 }
 
 exports['buildcop app xunitXML keeps an issue open for a passing test that failed in the same build (comment) 1'] = {
@@ -153,7 +153,7 @@ exports['buildcop app xunitXML keeps an issue open for a passing test that faile
 }
 
 exports['buildcop app xunitXML keeps an issue open for a passing test that failed in the same build (comment) 2'] = {
-  "body": "Looks like this issue is flaky. :worried:\n\nI'm going to leave this open and stop commenting.\n\nA human should fix and close this.\n\n---\n\nbuildID: 123\nbuildURL: http://example.com\nstatus: passed"
+  "body": "Looks like this issue is flaky. :worried:\n\nI'm going to leave this open and stop commenting.\n\nA human should fix and close this.\n\n---\n\ncommit: 123\nbuildURL: http://example.com\nstatus: passed"
 }
 
 exports['buildcop app xunitXML keeps an issue open for a passing test that failed in the same build (issue body) 1'] = {
@@ -168,9 +168,9 @@ exports['buildcop app xunitXML keeps an issue open for a passing test that faile
 }
 
 exports['buildcop app xunitXML keeps an issue open for a passing test that failed in the same build (issue body) 2'] = {
-  "body": "Looks like this issue is flaky. :worried:\n\nI'm going to leave this open and stop commenting.\n\nA human should fix and close this.\n\n---\n\nbuildID: 123\nbuildURL: http://example.com\nstatus: passed"
+  "body": "Looks like this issue is flaky. :worried:\n\nI'm going to leave this open and stop commenting.\n\nA human should fix and close this.\n\n---\n\ncommit: 123\nbuildURL: http://example.com\nstatus: passed"
 }
 
 exports['buildcop app xunitXML does not comment about failure on existing issue labeled quiet 1'] = {
-  "body": "buildID: 123\nbuildURL: http://example.com\nstatus: failed"
+  "body": "commit: 123\nbuildURL: http://example.com\nstatus: failed"
 }
