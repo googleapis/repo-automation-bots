4b2994dcb3d91242989bdd981e469e4b0d9b7de6
Sofia Leon <sofialeon@google.com>
2020-04-14 21:41:52 -0700

    fix: bug that won't let subtests pass by adding in regex

--- packages/merge-on-green/test/merge-on-green.ts
+++ packages/merge-on-green/test/merge-on-green.ts
@@ -131,7 +131,7 @@ function getPR(mergeable: boolean, mergeableState: string, state: string) {
     });
 }
 
-describe('merge-on-green-', () => {
+describe('merge-on-green', () => {
   let probot: Probot;
 
   beforeEach(() => {
@@ -324,6 +324,57 @@ describe('merge-on-green-', () => {
       scopes.forEach(s => s.done());
     });
 
+    it.only('rejects status checks that do not match the required check', async () => {
+      const scopes = [
+        getPR(true, 'clean', 'open'),
+        getBranchProtection(["this is what we're looking for"]),
+        getReviewsCompleted([
+          { user: { login: 'octocat' }, state: 'APPROVED' },
+        ]),
+        getLatestCommit([{ sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e' }]),
+        getMogLabel([{ name: 'automerge' }]),
+        getStatusi('6dcb09b5b57875f334f61aebed695e2e4193db5e', [
+          { state: 'success', context: "this is what we're looking fo" },
+        ]),
+      ];
+
+      await probot.receive({
+        name: 'schedule.repository',
+        payload: { org: 'testOwner' },
+        id: 'abc123',
+      });
+
+      scopes.forEach(s => s.done());
+    });
+
+    it.only('accepts status checks that match the beginning of the required status check', async () => {
+      const scopes = [
+        getPR(true, 'clean', 'open'),
+        getBranchProtection(["this is what we're looking for"]),
+        getReviewsCompleted([
+          { user: { login: 'octocat' }, state: 'APPROVED' },
+        ]),
+        getLatestCommit([{ sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e' }]),
+        getMogLabel([{ name: 'automerge' }]),
+        getStatusi('6dcb09b5b57875f334f61aebed695e2e4193db5e', [
+          {
+            state: 'success',
+            context: "this is what we're looking for/subtest",
+          },
+        ]),
+        merge(),
+      ];
+
+      await probot.receive({
+        name: 'schedule.repository',
+        payload: { org: 'testOwner' },
+        id: 'abc123',
+      });
+
+      scopes.forEach(s => s.done());
+    });
+
+  
     it('fails if no one has reviewed the PR', async () => {
       const scopes = [
         getPR(true, 'clean', 'open'),
