efa714374e7ad61b0d7c5cf0f2ac4fc25c5fb92b
Aziz Sonawalla <asonawalla@google.com>
2020-07-23 15:28:14 -0400

    add stubs for github processor

--- packages/monitoring-system/data-processor/src/data-processors/github-data-processor.ts
+++ packages/monitoring-system/data-processor/src/data-processors/github-data-processor.ts
@@ -14,8 +14,43 @@
 //
 import {DataProcessor} from './data-processor-abstract';
 
+interface GitHubRepository {
+  name: string,
+  owner: string
+}
+
+interface GitHubEvent {
+  payloadHash: string,
+  repository: GitHubRepository,
+  event_type: string,
+  timestamp: number,
+  actor: string
+}
+
+/**
+ * Collects and processes Events data from GitHub
+ */
 export class GitHubProcessor extends DataProcessor {
+  
   public async collectAndProcess(): Promise<void> {
     throw new Error('Method not implemented.');
   }
+
+  private async listRepositories(): Promise<GitHubRepository[]> {
+    throw new Error('Method not implemented.');
+  }
+
+  private async getPublicEventsForRepository(repository: GitHubRepository): Promise<GitHubEvent[]> {
+    // https://octokit.github.io/rest.js/v18#activity-list-repo-events
+    throw new Error('Method not implemented.');
+  }
+
+  private async storeEventsData(events: GitHubEvent[]): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
+  private iso8601TimeToEpoch(iso8601Timestamp: string): number {
+    throw new Error('Method not implemented.');
+  }
+
 }
