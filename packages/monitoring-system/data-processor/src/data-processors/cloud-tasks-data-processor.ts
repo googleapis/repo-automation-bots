4f928b100586a3c2d69acb846e6081c0b9f03038
Aziz Sonawalla <35513492+azizsonawalla@users.noreply.github.com>
2020-07-27 15:36:09 -0400

    feat(monitoring-system): add a data processor for Cloud Tasks data (#731)
    Add a data processor for Cloud Tasks data to monitoring-system/data-processor.
    
    Also adds a `MockFirestore` client and `MockCloudTasksClient`, where the former can be reused for testing other data processors too.
    
    Note: I'll be adding integration tests (that will use [Firestore Emulator](https://github.com/firebase/quickstart-testing)) in another PR -- thought it would be too big for this PR. I'm not using it for the current unit tests because AFAIK:
    (1) it can only test security rules and access -- so can't purposefully make it throw things like connection errors, etc
    (2) it needs to be spun-up as a separate application before running tests and that seems odd to do for unit tests

--- packages/monitoring-system/data-processor/src/data-processors/cloud-tasks-data-processor.ts
+++ packages/monitoring-system/data-processor/src/data-processors/cloud-tasks-data-processor.ts
@@ -12,10 +12,173 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-import {DataProcessor} from './data-processor-abstract';
+import {DataProcessor, ProcessorOptions} from './data-processor-abstract';
+import {WriteResult} from '@google-cloud/firestore';
+import {CloudTasksClient, protos, v2} from '@google-cloud/tasks';
 
+type CloudTasksList = [
+  protos.google.cloud.tasks.v2.ITask[],
+  protos.google.cloud.tasks.v2.IListTasksRequest | null,
+  protos.google.cloud.tasks.v2.IListTasksResponse
+];
+
+interface QueueStatus {
+  [queueName: string]: number;
+}
+
+export interface CloudTasksProcessorOptions extends ProcessorOptions {
+  tasksClient?: v2.CloudTasksClient;
+  taskQueueProjectId: string;
+  taskQueueLocation: string;
+}
+
+interface BotDocument {
+  bot_name: string;
+}
+
+interface TaskQueueDocument {
+  timestamp: number;
+  queue_name: string;
+  in_queue: number;
+}
+
+/**
+ * Collects the current status of Cloud Task queues associated
+ * with repo automation bots and inserts it into Firestore
+ */
 export class CloudTasksProcessor extends DataProcessor {
+  private projectId: string;
+  private location: string;
+  private tasksClient: v2.CloudTasksClient;
+
+  /**
+   * Create a Cloud Tasks data processor
+   * @param firestore (optional) a custom Firestore client
+   * @param tasksClient (optional) a custom Cloud Tasks client
+   */
+  constructor(options: CloudTasksProcessorOptions) {
+    super(options);
+    this.projectId = options.taskQueueProjectId;
+    this.location = options.taskQueueLocation;
+    this.tasksClient = options.tasksClient || new CloudTasksClient();
+  }
+
+  public getTasksProjectId(): string {
+    return this.projectId;
+  }
+
+  public getTasksProjectLocation(): string {
+    return this.location;
+  }
+
   public async collectAndProcess(): Promise<void> {
-    throw new Error('Method not implemented.');
+    return new Promise<void>((resolve, reject) => {
+      this.getBotNames()
+        .then(botNames => {
+          const queueNames = botNames.map(this.botNameToQueueName);
+          return this.getTaskQueueStatus(
+            this.projectId,
+            this.location,
+            queueNames
+          );
+        })
+        .then(queueStatus => {
+          return this.storeTaskQueueStatus(queueStatus);
+        })
+        .then(() => resolve())
+        .catch(error => {
+          console.trace(error);
+          reject(`Failed to collect and process Cloud Tasks data: ${error}`);
+        });
+    });
+  }
+
+  private async getBotNames(): Promise<string[]> {
+    return new Promise<string[]>((resolve, reject) => {
+      this.firestore
+        .collection('Bot')
+        .get()
+        .then(botCollection => {
+          if (!botCollection) {
+            reject('Got falsy results from Firestore');
+            return;
+          }
+          const botDocuments: BotDocument[] = botCollection.docs.map(
+            doc => doc.data() as BotDocument
+          );
+          resolve(botDocuments.map(doc => doc.bot_name));
+        })
+        .catch(error => {
+          console.trace(error);
+          reject(error);
+        });
+    });
+  }
+
+  private async getTaskQueueStatus(
+    project: string,
+    location: string,
+    queueNames: string[]
+  ): Promise<QueueStatus> {
+    return new Promise<QueueStatus>((resolve, reject) => {
+      const queueStatus: QueueStatus = {};
+
+      const taskListPromises: Promise<void>[] = queueNames.map(queueName => {
+        const queuePath = this.tasksClient.queuePath(
+          project,
+          location,
+          queueName
+        );
+        return this.tasksClient
+          .listTasks({parent: queuePath})
+          .then((taskList: CloudTasksList) => {
+            queueStatus[queueName] = taskList[0].length;
+          })
+          .catch(error => reject(error));
+      });
+
+      // Note: this will reject if even one of the calls fails
+      Promise.all(taskListPromises)
+        .then(() => {
+          resolve(queueStatus);
+        })
+        .catch(error => {
+          console.trace(error);
+          reject(error);
+        });
+    });
+  }
+
+  private async storeTaskQueueStatus(
+    queueStatus: QueueStatus
+  ): Promise<number> {
+    const currentTimestamp = new Date().getTime();
+    const collectionRef = this.firestore.collection('Task_Queue_Status');
+    const queueNames = Object.keys(queueStatus);
+
+    const writePromises: Promise<WriteResult>[] = queueNames.map(queueName => {
+      const documentKey = `${queueName}_${currentTimestamp}`;
+      const documentData: TaskQueueDocument = {
+        timestamp: currentTimestamp,
+        queue_name: queueName,
+        in_queue: queueStatus[queueName],
+      };
+      return collectionRef.doc(documentKey).set(documentData);
+    });
+
+    return new Promise((resolve, reject) => {
+      Promise.all(writePromises)
+        .then(() => resolve(currentTimestamp))
+        .catch(error => {
+          console.trace(error);
+          reject(error);
+        });
+    });
+  }
+
+  // Assumes that queue name == bot name with the exception
+  // that all "_" (underscore) are replaced by "-" (hyphen)
+  private botNameToQueueName(botName: string): string {
+    return botName.replace(/_/gi, '-');
   }
 }
