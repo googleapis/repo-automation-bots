92e6e358dab106a4cb2d3653f1244bf34c8f5e83
Harshitha Tadinada <sree.harshitha25@gmail.com>
2020-07-30 14:29:26 -0400

    feat: Determines if issue applies to and is compliant with slo rules and handles OOSLO labeling (#661)
    * feature: Slo-status-label bot currently with linting feature
    
    * fix: Added license header to snapshot file
    
    * fixing the create check text parameter
    
    * updated snapshot and removed unnecessary import
    
    * fix: Fixed the changes mentioned in comments
    
    * Fixed formating errors
    
    * Removed sandbox and added stub
    
    * fix: added changes to file
    
    * fix: removing appliesTo code that accidentally got commited
    
    * fix: Added check for if repo=.github and file present lint
    
    * feature: applies To func, org/repo defualt file ,& tests
    
    * fix: Build includes data now and fixed the new line errors
    
    * Delete schema.json
    
    * fix: New line errors
    
    * fix: Added new line spaces
    
    * fix: Removed octokit dependency from package and changed some of the error handling
    
    * fix: made a few changes to code to make it cleaner
    
    * fix: removed extra dep & made code cleaner
    
    * feature: checks issues and determines which slos it applies to or not
    
    * fix: used labels from payload instead of making API call
    
    * added interface for slo rules
    
    * fixed formatting
    
    * Added complaince settings logic
    
    * feat: checks if issue is in or out of slo and places label on OOSLO issues
    
    * fixing style
    
    * Added missing descriptions to schema
    
    * fix:Removed excess event files and cleaned up code
    
    * rearranged fles for readability
    
    * rearrangd test files and added new tests
    
    * Added comments
    
    * renamed slo-status-label to slo-bot for readability
    
    * changed npm start to run slo-bot and added log statments to keep track which rule slo is not compliant with
    
    * Updated schema with default write contributer
    
    * fix: Fixed logging comments to include issue number and repo name
    
    * fix: added valid headers
    
    * Fix: modified packages and syntax
    
    * changes in default responders logic, valid response time logic
    
    * changed the exporting statments of methods
    
    * Takes care of pull request ooslo labeling based on additional webhook actions, small readability fixes
    
    * fix: removed unecessary exports
    
    * change: changed logic for getting label name config
    
    * fix modified readme
    
    * fix: changes to function names and added extra logging
    
    * fix: Added test case for default config file
    
    * upgraded gcf-utils & typescript linter fix
    
    * fix: split slo logic into appliesTo and isCompliant for readability
    
    * fix: Added webhook to get issue comments event and changed naming conventions of files
    
    * changed some comments for defining method's purpose
    
    * fix: added gcf logger instead of console
    
    * fix: changed slo schema to not have priority and issueType. Also redesigned code of slo-appliesTo
    
    Co-authored-by: Colin Nelson <orthros@users.noreply.github.com>

---
+++ packages/slo-stat-bot/test/slo-label.ts
@@ -0,0 +1,193 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//s
+
+import {resolve} from 'path';
+// eslint-disable-next-line node/no-extraneous-import
+import {Probot} from 'probot';
+import nock from 'nock';
+import * as fs from 'fs';
+import {describe, it, beforeEach, afterEach} from 'mocha';
+
+// eslint-disable-next-line node/no-extraneous-import
+import Webhooks from '@octokit/webhooks';
+import snapshot from 'snap-shot-it';
+import handler from '../src/slo-bot';
+import sinon from 'sinon';
+import * as sloAppliesTo from '../src/slo-appliesTo';
+import * as sloCompliant from '../src/slo-compliant';
+
+nock.disableNetConnect();
+
+const fixturesPath = resolve(__dirname, '../../test/fixtures');
+
+describe('slo-label', () => {
+  let probot: Probot;
+
+  beforeEach(() => {
+    probot = new Probot({
+      // use a bare instance of octokit, the default version
+      // enables retries which makes testing difficult.
+      // eslint-disable-next-line node/no-extraneous-require
+      Octokit: require('@octokit/rest'),
+    });
+
+    probot.app = {
+      getSignedJsonWebToken() {
+        return 'abc123';
+      },
+      getInstallationAccessToken(): Promise<string> {
+        return Promise.resolve('abc123');
+      },
+    };
+    probot.load(handler);
+  });
+  describe('handle_labels', () => {
+    const config = fs.readFileSync(
+      resolve(fixturesPath, 'config', 'slo-stat-bot.yaml')
+    );
+
+    let payload: Webhooks.WebhookPayloadPullRequest;
+    let appliesToStub: sinon.SinonStub;
+    let isCompliantStub: sinon.SinonStub;
+
+    beforeEach(() => {
+      appliesToStub = sinon.stub(sloAppliesTo, 'doesSloApply');
+      isCompliantStub = sinon.stub(sloCompliant, 'isIssueCompliant');
+    });
+
+    afterEach(() => {
+      nock.cleanAll;
+      sinon.restore();
+    });
+    it('defaults to rotating light bulb emoji label if config yaml file is missing', async () => {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      payload = require(resolve(fixturesPath, 'events', 'issue_opened'));
+      const requests = nock('https://api.github.com')
+        .get('/repos/testOwner/testRepo/contents/.github/issue_slo_rules.json')
+        .reply(200, {
+          content:
+            'WwogICAgewogICAgICAgICJhcHBsaWVzVG8iOiB7CiAgICAgICAgICAgICJn\naXRIdWJMYWJlbHMiOiBbInByaW9yaXR5OiBQMiIsICJidWciXQogICAgICAg\nIH0sCiAgICAgICAgImNvbXBsaWFuY2VTZXR0aW5ncyI6IHsKICAgICAgICAg\nICAgInJlc3BvbnNlVGltZSI6IDAKICAgICAgICB9CiAgICB9CiBdCiAKIAog\nCiAK\n',
+        })
+        .get('/repos/testOwner/testRepo/contents/.github/slo-stat-bot.yaml')
+        .reply(404);
+
+      appliesToStub.onCall(0).returns(true);
+      isCompliantStub.onCall(0).returns(true);
+      await probot.receive({
+        name: 'issues.opened',
+        payload,
+        id: 'abc123',
+      });
+
+      requests.done();
+    });
+    it('labels ooslo if issue is not compliant and is missing ooslo label', async () => {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      payload = require(resolve(fixturesPath, 'events', 'issue_opened'));
+      const requests = nock('https://api.github.com')
+        .get('/repos/testOwner/testRepo/contents/.github/issue_slo_rules.json')
+        .reply(200, {
+          content:
+            'WwogICAgewogICAgICAgICJhcHBsaWVzVG8iOiB7CiAgICAgICAgICAgICJn\naXRIdWJMYWJlbHMiOiBbInByaW9yaXR5OiBQMiIsICJidWciXQogICAgICAg\nIH0sCiAgICAgICAgImNvbXBsaWFuY2VTZXR0aW5ncyI6IHsKICAgICAgICAg\nICAgInJlc3BvbnNlVGltZSI6IDAKICAgICAgICB9CiAgICB9CiBdCiAKIAog\nCiAK\n',
+        })
+        .get('/repos/testOwner/testRepo/contents/.github/slo-stat-bot.yaml')
+        .reply(200, {content: config.toString('base64')})
+        .post('/repos/testOwner/testRepo/issues/5/labels', body => {
+          snapshot(body);
+          return true;
+        })
+        .reply(200);
+
+      appliesToStub.onCall(0).returns(true);
+      isCompliantStub.onCall(0).returns(false);
+      await probot.receive({
+        name: 'issues.opened',
+        payload,
+        id: 'abc123',
+      });
+
+      requests.done();
+    });
+    it('does not relabel ooslo if issue is not compliant and has ooslo label', async () => {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      payload = require(resolve(fixturesPath, 'events', 'issue_ooslo'));
+      const requests = nock('https://api.github.com')
+        .get('/repos/testOwner/testRepo/contents/.github/issue_slo_rules.json')
+        .reply(200, {
+          content:
+            'WwogICAgewogICAgICAgICJhcHBsaWVzVG8iOiB7CiAgICAgICAgICAgICJn\naXRIdWJMYWJlbHMiOiBbInByaW9yaXR5OiBQMiIsICJidWciXQogICAgICAg\nIH0sCiAgICAgICAgImNvbXBsaWFuY2VTZXR0aW5ncyI6IHsKICAgICAgICAg\nICAgInJlc3BvbnNlVGltZSI6IDAKICAgICAgICB9CiAgICB9CiBdCiAKIAog\nCiAK\n',
+        })
+        .get('/repos/testOwner/testRepo/contents/.github/slo-stat-bot.yaml')
+        .reply(200, {content: config.toString('base64')});
+
+      appliesToStub.onCall(0).returns(true);
+      isCompliantStub.onCall(0).returns(false);
+      await probot.receive({
+        name: 'issues.opened',
+        payload,
+        id: 'abc123',
+      });
+
+      requests.done();
+    });
+    it('does not label ooslo if issue is compliant', async () => {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      payload = require(resolve(fixturesPath, 'events', 'issue_opened'));
+
+      const requests = nock('https://api.github.com')
+        .get('/repos/testOwner/testRepo/contents/.github/issue_slo_rules.json')
+        .reply(200, {
+          content:
+            'WwogICAgewogICAgICAgICJhcHBsaWVzVG8iOiB7CiAgICAgICAgICAgICJn\naXRIdWJMYWJlbHMiOiBbInByaW9yaXR5OiBQMiIsICJidWciXQogICAgICAg\nIH0sCiAgICAgICAgImNvbXBsaWFuY2VTZXR0aW5ncyI6IHsKICAgICAgICAg\nICAgInJlc3BvbnNlVGltZSI6IDAKICAgICAgICB9CiAgICB9CiBdCiAKIAog\nCiAK\n',
+        })
+        .get('/repos/testOwner/testRepo/contents/.github/slo-stat-bot.yaml')
+        .reply(200, {content: config.toString('base64')});
+
+      appliesToStub.onCall(0).returns(true);
+      isCompliantStub.onCall(0).returns(true);
+      await probot.receive({
+        name: 'issues.opened',
+        payload,
+        id: 'abc123',
+      });
+
+      requests.done();
+    });
+    it('removes ooslo label if issue is compliant and has ooslo label', async () => {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      payload = require(resolve(fixturesPath, 'events', 'issue_ooslo'));
+      const requests = nock('https://api.github.com')
+        .get('/repos/testOwner/testRepo/contents/.github/issue_slo_rules.json')
+        .reply(200, {
+          content:
+            'WwogICAgewogICAgICAgICJhcHBsaWVzVG8iOiB7CiAgICAgICAgICAgICJn\naXRIdWJMYWJlbHMiOiBbInByaW9yaXR5OiBQMiIsICJidWciXQogICAgICAg\nIH0sCiAgICAgICAgImNvbXBsaWFuY2VTZXR0aW5ncyI6IHsKICAgICAgICAg\nICAgInJlc3BvbnNlVGltZSI6IDAKICAgICAgICB9CiAgICB9CiBdCiAKIAog\nCiAK\n',
+        })
+        .delete('/repos/testOwner/testRepo/issues/5/labels/ooslo')
+        .reply(200)
+        .get('/repos/testOwner/testRepo/contents/.github/slo-stat-bot.yaml')
+        .reply(200, {content: config.toString('base64')});
+
+      appliesToStub.onCall(0).returns(true);
+      isCompliantStub.onCall(0).returns(true);
+      await probot.receive({
+        name: 'issues.opened',
+        payload,
+        id: 'abc123',
+      });
+
+      requests.done();
+    });
+  });
+});
