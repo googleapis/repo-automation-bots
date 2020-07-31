c4ed627fac8e2feb5cf002dd3b809f286092f3d8
Aziz Sonawalla <35513492+azizsonawalla@users.noreply.github.com>
2020-07-31 16:58:04 -0400

    refactor(gcf-utils): move GCFLogger and buildTriggerInfo to separate files to ensure readability of code (#784)
    Moved GCFLogger to separate file, and also moved the code to build trigger info to a separate file. Separated the tests as well.

--- packages/gcf-utils/test/logging-octokit-plugin.ts
+++ packages/gcf-utils/test/logging-octokit-plugin.ts
@@ -17,7 +17,7 @@ import {describe, beforeEach, it} from 'mocha';
 import {Octokit} from '@octokit/rest';
 import assert from 'assert';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const LoggingOctokitPlugin = require('../src/logging-octokit-plugin.js');
+const LoggingOctokitPlugin = require('../src/logging/logging-octokit-plugin.js');
 
 interface LogStatement {
   [key: string]: string | number | LogStatement;
