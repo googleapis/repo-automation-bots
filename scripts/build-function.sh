622ea443bece4049613894e1228a9d4216bb3535
Jeff Ching <chingor@google.com>
2020-08-17 17:14:51 -0700

    build: add trigger sync script
    build: split deployment
    
    build: fix triggers command
    
    build: fix typo
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    build: trigger run needs branch
    
    build: fix directory

--- scripts/build-function.sh
+++ scripts/build-function.sh
@@ -1,4 +1,6 @@
-# Copyright 2019 Google LLC
+#!/bin/bash
+
+# Copyright 2020 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,16 +14,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-steps:
-  - name: gcr.io/cloud-builders/gcloud
-    id: "sync-triggers"
-    entrypoint: bash
-    args:
-      - "-e"
-      - "./scripts/sync-triggers.sh"
-      - "$PROJECT_ID"
-      - "$_BUCKET"
-      - "$_FUNCTION_REGION"
-      - "$_KEY_RING"
-      - "$_REGION"
-      - "$_SCHEDULER_SERVICE_ACCOUNT_EMAIL"
+# compile the specified npm package and stage it in into target/<botname>
+
+set -eo pipefail
+
+if [[ $# -ne 1 ]]
+then
+  echo "Usage $0 <botDirectory>"
+  exit 1
+fi
+
+directoryName=$1
+botName=$(echo ${directoryName} | rev | cut -d/ -f1 | rev)
+
+workdir=$(pwd)
+targetDir="${workdir}/targets/${botName}"
+mkdir -p ${targetDir}
+
+pushd ${directoryName}
+
+# compile
+npm install -g typescript
+npm install
+npm run compile
+cp -r build ${targetDir}
+cp package.json "${targetDir}/package.json"
+
+popd
