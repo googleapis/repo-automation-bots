95a9c86e82487f644b497f5ab03267b6b5fbef25
Sofia Leon <sofialeon@google.com>
2020-10-14 00:37:46 -0700

    cut down on the issuslist item

--- packages/buildcop/src/buildcop.ts
+++ packages/buildcop/src/buildcop.ts
@@ -79,137 +79,21 @@ const GROUPED_MESSAGE = `Many tests failed at the same time in this package.
 
 `;
 
+// Octokit types only exports IssuesListForRepoResponseData, which is an array of
+// IssueListForRepoResponseItems. So, we're declaring this type for whenever we
+// need to refer to a single item rather than an array.
+
 interface IssuesListForRepoResponseItem {
   id: number;
-  node_id: string;
-  url: string;
-  repository_url: string;
-  labels_url: string;
-  comments_url: string;
-  events_url: string;
-  html_url: string;
   number: number;
   state: string;
   title: string;
   body: string;
-  user: {
-    login: string;
-    id: number;
-    node_id: string;
-    avatar_url: string;
-    gravatar_id: string;
-    url: string;
-    html_url: string;
-    followers_url: string;
-    following_url: string;
-    gists_url: string;
-    starred_url: string;
-    subscriptions_url: string;
-    organizations_url: string;
-    repos_url: string;
-    events_url: string;
-    received_events_url: string;
-    type: string;
-    site_admin: boolean;
-  };
   labels: {
-    id: number;
-    node_id: string;
-    url: string;
     name: string;
-    description: string;
-    color: string;
-    default: boolean;
   }[];
-  assignee: {
-    login: string;
-    id: number;
-    node_id: string;
-    avatar_url: string;
-    gravatar_id: string;
-    url: string;
-    html_url: string;
-    followers_url: string;
-    following_url: string;
-    gists_url: string;
-    starred_url: string;
-    subscriptions_url: string;
-    organizations_url: string;
-    repos_url: string;
-    events_url: string;
-    received_events_url: string;
-    type: string;
-    site_admin: boolean;
-  };
-  assignees: {
-    login: string;
-    id: number;
-    node_id: string;
-    avatar_url: string;
-    gravatar_id: string;
-    url: string;
-    html_url: string;
-    followers_url: string;
-    following_url: string;
-    gists_url: string;
-    starred_url: string;
-    subscriptions_url: string;
-    organizations_url: string;
-    repos_url: string;
-    events_url: string;
-    received_events_url: string;
-    type: string;
-    site_admin: boolean;
-  }[];
-  milestone: {
-    url: string;
-    html_url: string;
-    labels_url: string;
-    id: number;
-    node_id: string;
-    number: number;
-    state: string;
-    title: string;
-    description: string;
-    creator: {
-      login: string;
-      id: number;
-      node_id: string;
-      avatar_url: string;
-      gravatar_id: string;
-      url: string;
-      html_url: string;
-      followers_url: string;
-      following_url: string;
-      gists_url: string;
-      starred_url: string;
-      subscriptions_url: string;
-      organizations_url: string;
-      repos_url: string;
-      events_url: string;
-      received_events_url: string;
-      type: string;
-      site_admin: boolean;
-    };
-    open_issues: number;
-    closed_issues: number;
-    created_at: string;
-    updated_at: string;
-    closed_at: string;
-    due_on: string;
-  };
   locked: boolean;
-  active_lock_reason: string;
-  comments: number;
-  pull_request: {
-    url: string;
-    html_url: string;
-    diff_url: string;
-    patch_url: string;
-  };
   closed_at: string;
-  created_at: string;
-  updated_at: string;
 }
 
 interface TestCase {
@@ -242,6 +126,7 @@ interface PubSubContext {
   payload: BuildCopPayload;
 }
 
+// meta comment about the 'any' here: https://github.com/octokit/webhooks.js/issues/277
 export function buildcop(app: Application) {
   app.on('pubsub.message' as any, async (context: PubSubContext) => {
     const owner = context.payload.organization?.login;
