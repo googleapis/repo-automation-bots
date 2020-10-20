907c2ca6df7224f2d226f5f3a133f4b051d34081
Jeff Ching <chingor@google.com>
2020-10-20 09:20:04 -0700

    feat(blunderbuss): support assign_prs_by config (#1043)
    Allows blunderbuss to use the same configuration structure for issues by labels for assigning PRs.

--- packages/blunderbuss/__snapshots__/blunderbuss.js
+++ packages/blunderbuss/__snapshots__/blunderbuss.js
@@ -1,59 +1,65 @@
 exports['Blunderbuss issue tests assigns opened issues with no assignees 1'] = {
-  'assignees': [
-    'issues1'
+  "assignees": [
+    "issues1"
   ]
 }
 
 exports['Blunderbuss issue tests assigns issue when correct label 1'] = {
-  'assignees': [
-    'issues1'
+  "assignees": [
+    "issues1"
   ]
 }
 
 exports['Blunderbuss pr tests assigns user to a PR when opened with no assignee 1'] = {
-  'assignees': [
-    'prs1'
+  "assignees": [
+    "prs1"
   ]
 }
 
 exports['Blunderbuss pr tests assigns issue when correct label 1'] = {
-  'assignees': [
-    'prs1'
+  "assignees": [
+    "prs1"
   ]
 }
 
 exports['Blunderbuss issue tests assigns blunderbuss labeled issue by label 1'] = {
-  'assignees': [
-    'bar_baz_user'
+  "assignees": [
+    "bar_baz_user"
   ]
 }
 
 exports['Blunderbuss issue tests assigns opened issue by label 1'] = {
-  'assignees': [
-    'foo_user'
+  "assignees": [
+    "foo_user"
   ]
 }
 
 exports['Blunderbuss issue tests assigns labeled issue by label 1'] = {
-  'assignees': [
-    'bar_baz_user'
+  "assignees": [
+    "bar_baz_user"
   ]
 }
 
 exports['Blunderbuss pr tests assigns user to a PR when opened with no assignee, ignoring assign_issues_by 1'] = {
-  'assignees': [
-    'prs1'
+  "assignees": [
+    "prs1"
   ]
 }
 
 exports['Blunderbuss issue tests expands teams for an issue 1'] = {
-  'assignees': [
-    'user123'
+  "assignees": [
+    "user123"
   ]
 }
 
 exports['Blunderbuss pr tests expands teams for a PR 1'] = {
-  'assignees': [
-    'user123'
+  "assignees": [
+    "user123"
+  ]
+}
+
+exports['Blunderbuss pr tests assigns pr by label 1'] = {
+  "assignees": [
+    "java-samples-reviewers"
   ]
 }
