1d0cdbc3aa6a0472c18f38a8b04a7b9a3191e736
Jeff Ching <chingor@google.com>
2020-12-08 21:53:07 -0800

    feat(failurechecker): track multiple failures and autoclose after success (#1233)
    * feat: accumulate all release failures into issue
    
    also, close the found issue if there are no more release failures
    
    * fix: ignore pull requests that are marked as published
    
    * test: fix tests
    
    * test: add tests for new scenarios
    
    * chore: fix lint
    
    * test: remove extra test arg and fix test names

--- packages/failurechecker/__snapshots__/failurechecker.js
+++ packages/failurechecker/__snapshots__/failurechecker.js
@@ -1,6 +1,6 @@
 exports['failurechecker opens an issue on GitHub if there exists a pending label > threshold 1'] = {
   "title": "Warning: a recent release failed",
-  "body": "The release PR #33 is still in a pending state after several hours",
+  "body": "The following release PRs may have failed:\n\n* #33",
   "labels": [
     "type: process"
   ]
@@ -8,8 +8,24 @@ exports['failurechecker opens an issue on GitHub if there exists a pending label
 
 exports['failurechecker opens an issue on GitHub if there exists a tagged label > threshold 1'] = {
   "title": "Warning: a recent release failed",
-  "body": "The release PR #33 is still in a pending state after several hours",
+  "body": "The following release PRs may have failed:\n\n* #33",
   "labels": [
     "type: process"
   ]
+}
+
+exports['failurechecker updates an issue with new failures 1'] = {
+  "body": "The following release PRs may have failed:\n\n* #33\n* #34"
+}
+
+exports['failurechecker opens an issue with multiple failures 1'] = {
+  "title": "Warning: a recent release failed",
+  "body": "The following release PRs may have failed:\n\n* #33\n* #34",
+  "labels": [
+    "type: process"
+  ]
+}
+
+exports['failurechecker closes a PR once failing releases are handled 1'] = {
+  "state": "closed"
 }
