554c885d74a3e4e15d144f4c901ef3463a0520d2
Jeffrey Rennie <rennie@google.com>
2021-01-14 15:50:41 -0800

    Fix the git clone urls.

--- packages/bazel-bot/docker-image/docker-main.sh
+++ packages/bazel-bot/docker-image/docker-main.sh
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-git clone https://github.com/googleapis/googleapis
-git clone https://github.com/googleapis/googleapis-gen
+git clone https://github.com/googleapis/googleapis.git
+git clone git@github.com:googleapis/googleapis-gen.git
 
 export GOOGLEAPIS=googleapis
 export GOOGLEAPIS_GEN=googleapis-gen
