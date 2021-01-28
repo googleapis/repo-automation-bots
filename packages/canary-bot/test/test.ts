5d6c2eadf40cf9f6f0eb2342a2d0510cc561fdaa
Sofia Leon <sofialeon@google.com>
2021-01-28 10:30:50 -0800

    fix headers

--- packages/canary-bot/test/test.ts
+++ packages/canary-bot/test/test.ts
@@ -1,4 +1,4 @@
-// Copyright 2020 Google LLC
+// Copyright 2021 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,15 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/* eslint-disable node/no-extraneous-import */
 
 import myProbotApp from '../src/canary-bot';
 import {resolve} from 'path';
 import {Probot, createProbot, ProbotOctokit} from 'probot';
 import nock from 'nock';
-import * as fs from 'fs';
 import {describe, it, beforeEach} from 'mocha';
-import * as assert from 'assert';
 
 nock.disableNetConnect();
 
