03588b376d2ffcf0e78bc09976ded81a97e97093
Jeffrey Rennie <rennie@google.com>
2021-02-03 16:06:07 -0800

    Remove use of any.

--- packages/owl-bot/src/config-files.ts
+++ packages/owl-bot/src/config-files.ts
@@ -27,7 +27,7 @@ export const owlBotLockPath = '.github/.OwlBot.lock.yaml';
 
 // Throws an exception if the object does not have the necessary structure.
 // Otherwise, returns the same object as an OwlBotLock.
-export function owlBotLockFrom(o: any): OwlBotLock {
+export function owlBotLockFrom(o: Record<string, any>): OwlBotLock {
   if (typeof o.docker !== 'object') {
     throw Error('lock file did not contain "docker" key');
   }
--- packages/owl-bot/src/core.ts
+++ packages/owl-bot/src/core.ts
@@ -324,6 +324,9 @@ export async function getOwlBotLock(
     'utf8'
   );
   const maybeOwlBotLock = load(configString);
+  if (maybeOwlBotLock == null || typeof maybeOwlBotLock !== 'object') {
+    throw Error('lock file did not parse as object');
+  }
   return owlBotLockFrom(maybeOwlBotLock);
 }
 
