60620d2a7aad60f4f275ebe1aeff09e48da6ca09
Sofia Leon <sofialeon@google.com>
2021-02-06 00:27:55 -0800

    fix(snippet-bot): upgrade to Probot 11

--- packages/snippet-bot/test/snippet-bot.ts
+++ packages/snippet-bot/test/snippet-bot.ts
@@ -53,11 +53,13 @@ describe('snippet-bot', () => {
 
   beforeEach(() => {
     probot = createProbot({
-      githubToken: 'abc123',
-      Octokit: ProbotOctokit.defaults({
-        retry: {enabled: false},
-        throttle: {enabled: false},
-      }),
+      overrides: {
+        githubToken: 'abc123',
+        Octokit: ProbotOctokit.defaults({
+          retry: {enabled: false},
+          throttle: {enabled: false},
+        }),
+      },
     });
     probot.load(myProbotApp);
   });
