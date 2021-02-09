657114ce129b5b1f317e0a6448d13eba85f7cd32
Benjamin E. Coe <bencoe@google.com>
2021-02-08 16:11:32 -0800

    feat: add logic to open pull request with updated  .OwlBot.lock.yaml (#1397)

---
+++ packages/owl-bot/src/bin/commands/open-pr.ts
@@ -0,0 +1,101 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Run like this:
+// node ./build/src/bin/owl-bot.js list-repos --docker-image foo
+
+import {readFile} from 'fs';
+import {ConfigsStore} from '../../database';
+import {createOnePullRequestForUpdatingLock} from '../../handlers';
+import {
+  getAuthenticatedOctokit,
+  getGitHubShortLivedAccessToken,
+} from '../../core';
+import {promisify} from 'util';
+import yargs = require('yargs');
+
+const readFileAsync = promisify(readFile);
+
+interface Args {
+  'pem-path': string;
+  'app-id': number;
+  installation: number;
+  'docker-image': string;
+  'docker-digest': string;
+  repo: string;
+}
+
+export const openPR: yargs.CommandModule<{}, Args> = {
+  command: 'open-pr',
+  describe: 'Open a pull request with an updated .OwlBot.lock.yaml',
+  builder(yargs) {
+    return yargs
+      .option('pem-path', {
+        describe: 'provide path to private key for requesting JWT',
+        type: 'string',
+        demand: true,
+      })
+      .option('app-id', {
+        describe: 'GitHub AppID',
+        type: 'number',
+        demand: true,
+      })
+      .option('installation', {
+        describe: 'installation ID for GitHub app',
+        type: 'number',
+        demand: true,
+      })
+      .option('docker-image', {
+        describe:
+          'The full path of the docker image that changed.  ex: gcr.io/repo-automation-bots/nodejs-post-processor',
+        type: 'string',
+        demand: true,
+      })
+      .option('docker-digest', {
+        describe: 'the docker digest sha',
+        type: 'string',
+        demand: true,
+      })
+      .option('repo', {
+        describe: 'repository to run against, e.g., googleapis/foo',
+        type: 'string',
+        demand: true,
+      });
+  },
+  async handler(argv) {
+    const privateKey = await readFileAsync(argv['pem-path'], 'utf8');
+    const token = await getGitHubShortLivedAccessToken(
+      privateKey,
+      argv['app-id'],
+      argv.installation
+    );
+    const fakeConfigStore = ({
+      findPullRequestForUpdatingLock: () => undefined,
+      recordPullRequestForUpdatingLock: () => {},
+    } as unknown) as ConfigsStore;
+    const octokit = await getAuthenticatedOctokit(token.token);
+    createOnePullRequestForUpdatingLock(
+      fakeConfigStore,
+      octokit,
+      argv.repo,
+      {
+        docker: {
+          image: argv['docker-image'],
+          digest: argv['docker-digest'],
+        },
+      },
+      new Date()
+    );
+  },
+};
---
+++ packages/owl-bot/src/handlers.ts
@@ -0,0 +1,135 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {createPullRequest} from 'code-suggester';
+import {dump} from 'js-yaml';
+import {OwlBotLock, owlBotLockPath} from './config-files';
+import {Configs, ConfigsStore} from './database';
+import {OctokitType} from './core';
+import {Octokit} from '@octokit/rest';
+
+/**
+ * Invoked when a new pubsub message arrives because a new post processor
+ * docker image has been published to Google Container Registry.
+ * @param db: database
+ * @param dockerImageName: the name of the docker image that was updated.
+ *   example: "gcr.io/repo-automation-bots/nodejs-post-processor:latest"
+ * @param dockerImageDigest: the new digest for the image.
+ *   example: "sha256:1245151230998"
+ */
+export async function onPostProcessorPublished(
+  configsStore: ConfigsStore,
+  octokit: OctokitType,
+  dockerImageName: string,
+  dockerImageDigest: string,
+  logger = console
+): Promise<void> {
+  // Examine all the repos that use the specified docker image for post
+  // processing.
+  const repos: [
+    string,
+    Configs
+  ][] = await configsStore.findReposWithPostProcessor(dockerImageName);
+  for (const [repo, configs] of repos) {
+    let stale = true;
+    // The lock file may be missing, for example when a new repo is created.
+    try {
+      stale = configs.lock!.docker.digest !== dockerImageDigest;
+    } catch (e) {
+      logger.log(repo + ' did not have a valid .OwlBot.yaml.lock file.');
+    }
+    if (stale) {
+      const lock: OwlBotLock = {
+        docker: {
+          digest: dockerImageDigest,
+          image: dockerImageName,
+        },
+      };
+      // TODO(bcoe): switch updatedAt to date from PubSub payload:
+      createOnePullRequestForUpdatingLock(
+        configsStore,
+        octokit,
+        repo,
+        lock,
+        new Date()
+      );
+    }
+  }
+}
+
+/**
+ * Creates a pull request to update .OwlBot.lock.yaml, if one doesn't already
+ * exist.
+ * @param db: database
+ * @param octokit: Octokit.
+ * @param repoFull: full repo name like "googleapis/nodejs-vision"
+ * @param lock: The new contents of the lock file.
+ * @returns: the uri of the new or existing pull request
+ */
+export async function createOnePullRequestForUpdatingLock(
+  configsStore: ConfigsStore,
+  octokit: OctokitType,
+  repoFull: string,
+  lock: OwlBotLock,
+  updatedAt: Date
+): Promise<string> {
+  const existingPullRequest = await configsStore.findPullRequestForUpdatingLock(
+    repoFull,
+    lock
+  );
+  if (existingPullRequest) {
+    return existingPullRequest;
+  }
+  const [owner, repo] = repoFull.split('/');
+  // createPullRequest expects file updates as a Map
+  // of objects with content/mode:
+  const changes = new Map();
+  changes.set(owlBotLockPath, {
+    content: dump(lock),
+    mode: '100644',
+  });
+  // Opens a pull request with any files represented in changes:
+  const prNumber = await createPullRequest(
+    octokit as Octokit,
+    changes,
+    {
+      upstreamOwner: owner,
+      upstreamRepo: repo,
+      // TODO(rennie): we should provide a context aware commit
+      // message for this:
+      title: 'chore: update OwlBot.lock with new version of post-processor',
+      branch: 'owl-bot-lock-1',
+      // TODO(bcoe): come up with a funny blurb to put in PRs.
+      description: `Version ${
+        lock.docker.digest
+      } was published at ${updatedAt.toISOString()}.`,
+      // TODO(rennie): we need a way to track what the primary branch
+      // is for a PR.
+      primary: 'main',
+      force: true,
+      fork: false,
+      // TODO(rennie): we should provide a context aware commit
+      // message for this:
+      message: 'Update OwlBot.lock',
+    },
+    {level: 'error'}
+  );
+  const newPullRequest = `https://github.com/${repoFull}/pull/${prNumber}`;
+  await configsStore.recordPullRequestForUpdatingLock(
+    repoFull,
+    lock,
+    newPullRequest
+  );
+  return newPullRequest;
+}
