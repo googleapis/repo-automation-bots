ab3a5df0e473b2426a49c00cf18f034c80c3d04a
sofisl <55454395+sofisl@users.noreply.github.com>
2021-02-10 13:46:04 -0800

    fix(do-not-merge): upgrade to Probot 11 (#1376)

--- packages/do-not-merge/test/test.ts
+++ packages/do-not-merge/test/test.ts
@@ -30,11 +30,13 @@ describe('do-not-merge', () => {
 
   beforeEach(() => {
     probot = createProbot({
-      githubToken: 'abc123',
-      Octokit: ProbotOctokit.defaults({
-        retry: {enabled: false},
-        throttle: {enabled: false},
-      }),
+      overrides: {
+        githubToken: 'abc123',
+        Octokit: ProbotOctokit.defaults({
+          retry: {enabled: false},
+          throttle: {enabled: false},
+        }),
+      },
     });
     probot.load(myProbotApp);
   });
