5f2925d7719e10f7e58aaebd4c8ca96f8f9397e6
sofisl <55454395+sofisl@users.noreply.github.com>
2021-02-10 16:48:19 -0800

    fix(blunderbuss): second attempt to upgrade blunderbuss to probot 11 (#1374)
    * fix(blunderbuss): second attempt at upgrading to Probot 11
    
    * fix(blunderbuss): upgrade to Probot 11
    
    * fix package json
    
    * revert auto label change
    
    * fix: broken tests
    
    * fix: add block
    
    Co-authored-by: Justin Beckwith <justin.beckwith@gmail.com>

--- packages/blunderbuss/src/blunderbuss.ts
+++ packages/blunderbuss/src/blunderbuss.ts
@@ -13,9 +13,10 @@
 // limitations under the License.
 
 // eslint-disable-next-line node/no-extraneous-import
-import {Application, Context} from 'probot';
+import {Probot, Context} from 'probot';
 import * as util from 'util';
 import {logger} from 'gcf-utils';
+import {EventPayloads} from '@octokit/webhooks';
 
 const CONFIGURATION_FILE_PATH = 'blunderbuss.yml';
 const ASSIGN_LABEL = 'blunderbuss: assign';
@@ -30,14 +31,6 @@ interface Configuration {
   assign_issues_by?: ByConfig[];
   assign_prs?: string[];
   assign_prs_by?: ByConfig[];
-}
-
-interface Issue {
-  owner: string;
-  repo: string;
-  number: number;
-  labels: {name: string}[];
-  draft?: boolean;
 }
 
 // Randomly returns an item from an array, while ignoring the provided value.
@@ -56,7 +49,15 @@ export const sleep = (ms: number) => {
   return new Promise(r => setTimeout(r, ms));
 };
 
-export function blunderbuss(app: Application) {
+function isIssue(
+  issue:
+    | EventPayloads.WebhookPayloadIssues
+    | EventPayloads.WebhookPayloadPullRequest
+): issue is EventPayloads.WebhookPayloadIssues {
+  return (issue as EventPayloads.WebhookPayloadIssues).issue !== undefined;
+}
+
+export function blunderbuss(app: Probot) {
   app.on(
     [
       'issues.opened',
@@ -83,33 +84,41 @@ export function blunderbuss(app: Application) {
       }
       config = config || {};
 
-      const issue: Issue =
-        context.payload.issue || context.payload.pull_request;
+      let issue: EventPayloads.WebhookPayloadIssuesIssue | undefined;
+      let pullRequest:
+        | EventPayloads.WebhookPayloadPullRequestPullRequest
+        | undefined;
+      if (isIssue(context.payload)) {
+        issue = context.payload.issue;
+      } else {
+        pullRequest = context.payload.pull_request;
+      }
       const repoName = context.payload.repository.full_name;
 
+      const issueOrPRNumber = issue?.number || pullRequest?.number;
+      const issueOrPROwner = issue?.user.login || pullRequest?.user.login;
+      const issueOrPRRepo = context.payload.repository.name;
       // If this is a PR, and it's in draft mode, don't assign it
-      if (issue.draft === true) {
-        logger.info(`Skipping ${repoName}#${issue.number} as it's a draft PR`);
+      if (pullRequest?.draft === true) {
+        logger.info(
+          `Skipping ${repoName}#${pullRequest.number} as it's a draft PR`
+        );
         return;
       }
 
       // Check if the config specifically asks to not assign issues or PRs
       if (
-        (context.payload.issue &&
-          !config.assign_issues &&
-          !config.assign_issues_by) ||
-        (context.payload.pull_request &&
-          !config.assign_prs &&
-          !config.assign_prs_by)
+        (issue && !config.assign_issues && !config.assign_issues_by) ||
+        (pullRequest && !config.assign_prs && !config.assign_prs_by)
       ) {
-        const paramName = context.payload.issue
+        const paramName = issue
           ? '"assign_issues" and "assign_issues_by"'
           : '"assign_prs" and "assign_prs_by"';
         context.log.info(
           util.format(
             '[%s] #%s ignored: %s not in config',
             repoName,
-            issue.number,
+            issueOrPRNumber,
             paramName
           )
         );
@@ -117,102 +126,103 @@ export function blunderbuss(app: Application) {
       }
 
       // PRs are a superset of issues, so we can handle them similarly.
-      const assignConfig = context.payload.issue
-        ? config.assign_issues
-        : config.assign_prs!;
-      const byConfig = context.payload.issue
-        ? config.assign_issues_by
-        : config.assign_prs_by;
-      const issuePayload =
-        context.payload.issue || context.payload.pull_request;
+      const assignConfig = issue ? config.assign_issues : config.assign_prs!;
+      const byConfig = issue ? config.assign_issues_by : config.assign_prs_by;
+      const issuePayload = issue || pullRequest;
 
       const isLabeled = context.payload.action === 'labeled';
       if (isLabeled) {
         // Only assign an issue that already has an assignee if labeled with
         // ASSIGN_LABEL.
         if (
-          context.payload.label.name !== ASSIGN_LABEL &&
-          issuePayload.assignees?.length
+          context.payload.label?.name !== ASSIGN_LABEL &&
+          issuePayload!.assignees?.length
         ) {
           context.log.info(
             '[%s] #%s ignored: incorrect label ("%s") because it is already assigned',
             repoName,
-            issue.number,
-            context.payload.label.name
+            issueOrPRNumber,
+            context.payload.label?.name
           );
           return;
         }
         // Check if the new label has a possible assignee.
         // Don't check all labels to avoid updating an old issue when someone
         // changes a random label.
-        const assigneesForNewLabel = findAssignees(byConfig, [
-          context.payload.label.name,
-        ]);
+        let assigneesForNewLabel: string[] | undefined;
+        if (context.payload.label?.name) {
+          assigneesForNewLabel = findAssignees(byConfig, [
+            context.payload.label.name,
+          ]);
+        }
+
         if (
-          assigneesForNewLabel.length === 0 &&
-          context.payload.label.name !== ASSIGN_LABEL
+          assigneesForNewLabel?.length === 0 &&
+          context.payload.label?.name !== ASSIGN_LABEL
         ) {
           context.log.info(
             '[%s] #%s ignored: incorrect label ("%s")',
             repoName,
-            issue.number,
-            context.payload.label.name
+            issueOrPRNumber,
+            context.payload.label?.name
           );
           return;
         }
-        if (context.payload.label.name === ASSIGN_LABEL) {
+        if (context.payload.label?.name === ASSIGN_LABEL) {
           // Remove the label so the user knows the event was processed (even if not successfully).
-          await context.github.issues.removeLabel(
+          await context.octokit.issues.removeLabel(
             context.issue({name: ASSIGN_LABEL})
           );
         }
       }
 
       // Allow the label to force a new assignee, even if one is already assigned.
-      if (!isLabeled && issuePayload.assignees.length !== 0) {
+      if (!isLabeled && issuePayload!.assignees.length !== 0) {
         context.log.info(
           util.format(
             '[%s] #%s ignored: already has assignee(s)',
             repoName,
-            issue.number
+            issueOrPRNumber
           )
         );
         return;
       }
 
-      let labels: string[] = [];
+      let labels: string[] | undefined = [];
       if (byConfig !== undefined && context.payload.action === 'opened') {
         // It is possible that blunderbuss is running before other bots have
         // a chance to add extra labels. Wait and re-pull fresh labels
         // before comparing against the config.
         await sleep(10_000);
-        const labelResp = await context.github.issues.listLabelsOnIssue({
-          issue_number: issue.number,
+        const labelResp = await context.octokit.issues.listLabelsOnIssue({
+          issue_number: issueOrPRNumber!,
           owner: context.payload.repository.owner.login,
           repo: context.payload.repository.name,
         });
         labels = labelResp.data.map(lr => lr.name);
       } else {
-        labels = issue.labels?.map(l => l.name);
+        labels = issue
+          ? issue.labels?.map(l => l.name)
+          : pullRequest?.labels?.map(l => l.name);
       }
       const preferredAssignees = findAssignees(byConfig, labels);
       let possibleAssignees = preferredAssignees.length
         ? preferredAssignees
         : assignConfig || [];
       possibleAssignees = await expandTeams(possibleAssignees, context);
-      const assignee = randomFrom(possibleAssignees, issuePayload.user.login);
+      const assignee = randomFrom(possibleAssignees, issuePayload!.user.login);
       if (!assignee) {
         context.log.info(
           util.format(
             '[%s] #%s not assigned: no valid assignee(s)',
             repoName,
-            issue.number
+            issueOrPRNumber
           )
         );
         return;
       }
 
-      const resp = await context.github.issues.addAssignees(
+      const resp = await context.octokit.issues.addAssignees(
         context.issue({assignees: [assignee]})
       );
       if (resp.status !== 201) {
@@ -220,7 +230,7 @@ export function blunderbuss(app: Application) {
           util.format(
             '[%s] #%s could not be assined to %s: status %d',
             repoName,
-            issue.number,
+            issueOrPRNumber,
             assignee,
             resp.status
           )
@@ -230,9 +240,9 @@ export function blunderbuss(app: Application) {
       context.log.info(
         util.format(
           '[%s] #%s was assigned to %s',
-          issue.owner,
-          issue.repo,
-          issue.number,
+          issueOrPROwner,
+          issueOrPRRepo,
+          issueOrPRNumber,
           assignee
         )
       );
@@ -242,7 +252,7 @@ export function blunderbuss(app: Application) {
 
 function findAssignees(
   config: ByConfig[] | undefined,
-  labels: string[]
+  labels: string[] | undefined
 ): string[] {
   let assignees: string[] = [];
   if (labels && config) {
@@ -271,13 +281,15 @@ async function expandTeams(
     // There is a slash. Probably a team.
     const [org, slug] = user.split('/');
     const members = (
-      await context.github.teams.listMembersInOrg({
+      await context.octokit.teams.listMembersInOrg({
         org,
         team_slug: slug,
       })
     ).data;
     for (const member of members) {
-      result.push(member.login);
+      if (member?.login) {
+        result.push(member?.login);
+      }
     }
   }
   return result;
