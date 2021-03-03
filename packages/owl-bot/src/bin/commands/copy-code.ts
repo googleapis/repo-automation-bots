744aa022d9058f682129d0c6aa3260fffd208558
Jeffrey Rennie <rennie@google.com>
2021-03-03 13:00:45 -0800

    feat: command line tool to copy source files from googleapis-gen to a… (#1454)
    * feat: command line tool to copy source files from googleapis-gen to a library repo
    
    * fix: more validation for .OwlBot.yaml
    
    * refactor: better name: toFullMatchRegExp()
    
    * feat: provide instructions to re-attempt failed copies
    
    * fix: call to copyCode
    
    * fix: add missing working directory to some cmd statements
    
    * fix: pass a title and body to octokit.pulls.create()
    
    * feat: accept a local path for sourceRepo
    
    Before this change, sourceRepo named a github repo like googleapis/googleapis-gen, and therefore always had to be cloned.  With this change, sourceRepo can specify a path to a local directory.
    
    This will reduce the number of times we need to clone the 200-MB googleapis-gen repo.
    
    * feat: add comments

---
+++ packages/owl-bot/src/bin/commands/copy-code.ts
@@ -0,0 +1,61 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// To Run: node ./build/src/bin/owl-bot.js copy-code <args>
+
+import tmp from 'tmp';
+import yargs = require('yargs');
+import {copyCode, loadOwlBotYaml} from '../../copy-code';
+
+interface Args {
+  'source-repo': string;
+  'source-repo-commit-hash': string;
+  dest: string | undefined;
+}
+
+export const copyCodeCommand: yargs.CommandModule<{}, Args> = {
+  command: 'copy-code',
+  describe: 'copies code from source to repo into a local repo',
+  builder(yargs) {
+    return yargs
+      .option('source-repo', {
+        describe: 'The source repository.  Example: googleapis/googleapis-gen',
+        type: 'string',
+        demand: false,
+        default: 'googleapis/googleapis-gen',
+      })
+      .option('source-repo-commit-hash', {
+        describe:
+          'The commit hash of the source repo from which to copy files.',
+        type: 'string',
+        demand: true,
+      })
+      .option('dest', {
+        describe:
+          'The directory containing a local repo.  Example: nodejs/vision.',
+        type: 'string',
+        demand: false,
+      });
+  },
+  async handler(argv) {
+    const destDir = argv.dest ?? process.cwd();
+    await copyCode(
+      argv['source-repo'],
+      argv['source-repo-commit-hash'],
+      destDir,
+      tmp.dirSync().name,
+      await loadOwlBotYaml(destDir)
+    );
+  },
+};
