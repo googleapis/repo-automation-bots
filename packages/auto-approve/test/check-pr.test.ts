84a97ec22d01e0b993ec8cfa8ca441c31f7c80db
Jeffrey Rennie <rennie@google.com>
2021-03-09 14:50:14 -0800

    launch.json for debugging

--- packages/auto-approve/test/check-pr.test.ts
+++ packages/auto-approve/test/check-pr.test.ts
@@ -10,6 +10,9 @@ import * as fs from 'fs';
 describe('check pr against config', () => {
     describe('checks that files match at least one of the patterns', () => {
         it('should return true if file matches at least one of the patterns', () => {
+            let n = 1;
+            n += 2;
+            assert.strictEqual(n, 3);
             // const pathsMatch = checkFilePathsMatch()
         });
     });
