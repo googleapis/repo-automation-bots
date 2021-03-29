0881bc45b7be193fe74e26e4fe8c9e4b877764c7
Jeff Ching <chingor@google.com>
2021-03-29 13:04:58 -0700

    feat: add trigger-sync cloud build docker image

---
+++ trigger-sync/README.md
@@ -0,0 +1,12 @@
+# Trigger Sync Cloud Build Image
+
+This docker image is designed to help you sync multiple build triggers within a single
+repository.
+
+## Usage
+
+Create a `cloudbuild.yaml` at the root of your repository:
+
+```yaml
+
+```
---
+++ trigger-sync/sync-triggers.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script ensures that a Cloud Build trigger exists for each non-root
+# cloudbuild.yaml file. Each is triggered on a push to master for any file
+# changed in the directory containing that cloudbuild.yaml file.
+
+set -eo pipefail
+
+if [ -z "${PROJECT_ID} "]
+then
+  echo "Need to set PROJECT_ID environment variable"
+  exit 1
+fi
+
+if [ -z "${REPO_NAME} "]
+then
+  echo "Need to set REPO_NAME environment variable"
+  exit 1
+fi
+
+echo $REPO_NAME
+
+if [[ $# -lt 1 ]]
+then
+  echo "Usage: $0 <substitutions>"
+  exit 1
+fi
+
+if [ -z "${BRANCH_NAME} "]
+then
+  BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+fi
+
+# propagate substitution variables to deploy triggers
+SUBSTITUTIONS=$1
+
+# find all non-root cloudbuild.yaml configs
+for config in $(find -- */ -name 'cloudbuild.yaml' | sort -u)
+do
+  directory=$(dirname "${config}")
+  triggerShortName=$(dirname "${config}" | rev | cut -d/ -f1 | rev)
+  triggerName=$(dirname "${config}" | sed 's/\//-/g')
+
+  # test to see if the deployment trigger already exists
+  if gcloud beta builds triggers describe "${triggerName}" --project="${project}" &>/dev/null
+  then
+    # trigger already exists, skip
+    continue
+  fi
+
+  echo "Syncing trigger for ${triggerShortName}"
+
+  # create the trigger
+  echo gcloud beta builds triggers create github \
+    --project="${PROJECT_ID}" \
+    --repo-name="${REPO_NAME}" \
+    --repo-owner="${REPO_OWNER}" \
+    --description="Trigger ${triggerShortName}" \
+    --included-files="${directory}/**" \
+    --name="${triggerName}" \
+    --branch-pattern="^${BRANCH_NAME}$" \
+    --build-config="${config}" \
+    --substitutions="${SUBSTITUTIONS},_DIRECTORY=${directory}"
+
+  # trigger the first deployment
+  echo gcloud beta builds triggers run "${triggerName}" \
+    --project="${PROJECT_ID}" \
+    --branch="${BRANCH_NAME}"
+done
