8d5fa4f0377cc755a657f0f423b3f87b1c4fdd8d
Viacheslav Rostovtsev <virost@google.com>
2021-04-15 22:35:57 +0000

    revert the example target

--- packages/bazel-bot/test-generate-googleapis-gen.sh
+++ packages/bazel-bot/test-generate-googleapis-gen.sh
@@ -66,8 +66,8 @@ git -C googleapis-gen checkout -b other
 # Test!
 export GOOGLEAPIS_GEN=`realpath googleapis-gen-clone`
 export INSTALL_CREDENTIALS="echo 'Pretending to install credentials...''"
-export BUILD_TARGETS="//google/cloud/vision/v1:google-cloud-vision-v1-csharp //google/bogus:api"
-export FETCH_TARGETS="//google/cloud/vision/v1:google-cloud-vision-v1-csharp"
+export BUILD_TARGETS="//google/cloud/vision/v1:vision-v1-nodejs //google/bogus:api"
+export FETCH_TARGETS="//google/cloud/vision/v1:vision-v1-nodejs"
 bash -x "$generate_googleapis_gen"
 
 # Display the state of googleapis-gen
