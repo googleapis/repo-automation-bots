e36796195847c59be405d3c5c6e50d0b0cbff04b
Jeffrey Rennie <rennie@google.com>
2021-04-22 20:58:32 -0700

    create DRAFT pull requests

--- packages/owl-bot/src/handlers.ts
+++ packages/owl-bot/src/handlers.ts
@@ -147,6 +147,8 @@ export async function createOnePullRequestForUpdatingLock(
       fork: false,
       // TODO(bcoe): replace this message with last commit to synthtool:
       message: 'build: update .OwlBot.lock with new version of post-processor',
+      draft: true,
+      labels: [core.UPDATE_LOCK_PULL_REQUEST_LABEL],
     },
     {level: 'error'}
   );
