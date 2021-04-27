a333a97c33529bb051b4b4485d2bda36076ec2c5
Takashi Matsuo <tmatsuo@google.com>
2021-04-27 18:51:25 +0000

    chore: license, correct comment format

--- metrics/snippet-bot-number-of-prevented-violations.sql
+++ metrics/snippet-bot-number-of-prevented-violations.sql
@@ -1,8 +1,24 @@
-# Number of violations which are prevented by snippet-bot.
+/**
+ * Copyright 2021 Google LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+-- Number of violations which are prevented by snippet-bot.
 select
     EXTRACT(DATE FROM t5.t AT TIME ZONE "America/Los_Angeles") as date,
 	t4.target as target, t4.type as type, t4.max_count - t5.latest_count as prevented_count from (
-        # t4 represents maximum count per violation type and pr
+        -- t4 represents maximum count per violation type and pr
         select
             t3.max_count as max_count,
             t3.target as target,
@@ -21,7 +37,7 @@ select
             ) as t3
         ) as t4
         inner join (
-            # t5 shows the latest count for violation type on prs
+            -- t5 shows the latest count for violation type on prs
             SELECT
                 t1.count as latest_count,
                 t1.target as target,
