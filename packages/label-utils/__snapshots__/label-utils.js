d867cd49a7f336bc40c435b26137e85ef9cf9a92
Takashi Matsuo <tmatsuo@google.com>
2021-05-20 16:40:02 +0000

    feat(label-utils)!: label-utils initial release (#1812)
    part of #1673

---
+++ packages/label-utils/__snapshots__/label-utils.js
@@ -0,0 +1,31 @@
+exports['label-utils creates a label 1'] = {
+  name: 'test label',
+  current_name: 'test label',
+  description: 'test description',
+  color: '436228',
+};
+
+exports['label-utils updates a label 1'] = {
+  current_name: 'test label',
+  description: 'test description',
+  color: '436228',
+};
+
+exports['label-utils survives when failed to create a label 1'] = {
+  name: 'test label',
+  current_name: 'test label',
+  description: 'test description',
+  color: '436228',
+};
+
+exports['label-utils survives when failed to update a label 1'] = {
+  current_name: 'test label',
+  description: 'test description',
+  color: '436228',
+};
+
+exports['label-utils updates a label when the old name is uppercase 1'] = {
+  current_name: 'test label',
+  description: 'test description',
+  color: '436228',
+};
