ecbec86ed2cb6d24e9df505888cfd47d65bf4301
Jeffrey Rennie <rennie@google.com>
2021-05-26 16:32:51 -0700

    fix: never create a PR with a title > 255 characters
    Fixes https://github.com/googleapis/repo-automation-bots/issues/1895

--- packages/owl-bot/src/create-pr.ts
+++ packages/owl-bot/src/create-pr.ts
@@ -15,6 +15,23 @@
 import {newCmd} from './cmd';
 import {OctokitType} from './octokit-util';
 
+/***
+ * Github will reject the pull request if the title is longer than 255
+ * characters.  This function will move characters from the title to the body
+ * if the title is too long.
+ */
+export function resplit(
+  title: string,
+  body: string
+): {title: string; body: string} {
+  if (title.length > 255) {
+    const splitIndex = 252; // 255 minus 3 dots
+    body = '...' + title.substring(splitIndex) + '\n\n' + body;
+    title = title.substring(0, splitIndex) + '...';
+  }
+  return {title, body};
+}
+
 export async function createPullRequestFromLastCommit(
   owner: string,
   repo: string,
@@ -32,16 +49,18 @@ export async function createPullRequestFromLastCommit(
   cmd(`git push origin ${branch}`, {cwd: localRepoDir});
 
   // Use the commit's subject and body as the pull request's title and body.
-  const title: string = cmd('git log -1 --format=%s', {
+  const commitSubject: string = cmd('git log -1 --format=%s', {
     cwd: localRepoDir,
   })
     .toString('utf8')
     .trim();
-  const body: string = cmd('git log -1 --format=%b', {
+  const commitBody: string = cmd('git log -1 --format=%b', {
     cwd: localRepoDir,
   })
     .toString('utf8')
     .trim();
+
+  const {title, body} = resplit(commitSubject, commitBody);
 
   // Create a pull request.
   const pull = await octokit.pulls.create({
---
+++ packages/owl-bot/test/create-pr.ts
@@ -0,0 +1,37 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {describe, it} from 'mocha';
+import * as assert from 'assert';
+import {resplit} from '../src/create-pr';
+
+describe('resplit', () => {
+  it('leaves a short title unchanged', () => {
+    const tb = resplit('title', 'body');
+    assert.deepStrictEqual(tb, {title: 'title', body: 'body'});
+  });
+
+  it('resplits a long title', () => {
+    const title =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    const tb = resplit(title, 'body');
+    assert.strictEqual(tb.title.length, 255);
+    assert.deepStrictEqual(tb, {
+      body:
+        '...r in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\nbody',
+      title:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolo...',
+    });
+  });
+});
