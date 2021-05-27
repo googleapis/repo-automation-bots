5e9cd99313d9f545d574f9bba55866959af2e701
Jeffrey Rennie <rennie@google.com>
2021-05-26 17:41:52 -0700

    fix: never create a PR with a title > 255 characters (#1897)
    * fix: never create a PR with a title > 255 characters
    
    Fixes https://github.com/googleapis/repo-automation-bots/issues/1895
    
    * fix: also truncate long bodies to < 64k

--- packages/owl-bot/src/create-pr.ts
+++ packages/owl-bot/src/create-pr.ts
@@ -15,6 +15,30 @@
 import {newCmd} from './cmd';
 import {OctokitType} from './octokit-util';
 
+// Limits imposed by Github.
+export const MAX_TITLE_LENGTH = 255;
+export const MAX_BODY_LENGTH = 64 * 1024 - 1;
+
+/***
+ * Github will reject the pull request if the title is longer than 255
+ * characters.  This function will move characters from the title to the body
+ * if the title is too long.
+ */
+export function resplit(
+  title: string,
+  body: string
+): {title: string; body: string} {
+  if (title.length > MAX_TITLE_LENGTH) {
+    const splitIndex = MAX_TITLE_LENGTH - 3; // 3 dots.
+    body = ('...' + title.substring(splitIndex) + '\n\n' + body).substring(
+      0,
+      MAX_BODY_LENGTH
+    );
+    title = title.substring(0, splitIndex) + '...';
+  }
+  return {title, body};
+}
+
 export async function createPullRequestFromLastCommit(
   owner: string,
   repo: string,
@@ -32,16 +56,18 @@ export async function createPullRequestFromLastCommit(
   cmd(`git push origin ${branch}`, {cwd: localRepoDir});
 
   // Use the commit's subject and body as the pull request's title and body.
-  const title: string = cmd('git log -1 --format=%s', {
+  const commitSubject: string = cmd('git log -1 --format=%s', {
     cwd: localRepoDir,
   })
     .toString('utf8')
     .trim();
-  const body: string = cmd('git log -1 --format=%b', {
+  const commitBody: string = cmd('git log -1 --format=%b', {
     cwd: localRepoDir,
   })
     .toString('utf8')
     .trim();
+
+  const {title, body} = resplit(commitSubject, commitBody);
 
   // Create a pull request.
   const pull = await octokit.pulls.create({
