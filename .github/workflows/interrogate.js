28108179a9410b859406041d295f065525fb84c7
Takashi Matsuo <tmatsuo@google.com>
2021-06-08 19:57:06 +0000

    chore: delete monitoring-system

--- .github/workflows/interrogate.js
+++ .github/workflows/interrogate.js
@@ -32,19 +32,11 @@ let goPaths = new Set();
 let bashPaths = new Set();
 for (const change of changes) {
   if (change.startsWith('packages/')) {
-    if (change.startsWith('packages/slo-stat-bot')) {
-      // Currently our test pipeline does not allow us to delete
-      // an existing bot. We may want to handle it better in the future,
-      // but this quick hack may work for #1952.
-      continue;
-    }
     if (change.startsWith('packages/monitoring-system')) {
-      // projects under packages/monitoring-system are nested, but ensure
-      // the files are in the nested directory
-      const parts = change.split('/');
-      if (parts.length > 3) {
-        nodePaths.add(`monitoring-system/${parts[2]}`);
-      }
+      // Currently our test pipeline does not allow us to delete an
+      // existing package. We may want to handle it better in the
+      // future.
+      continue;
     } else {
       nodePaths.add(change.split('/')[1]);
     }
