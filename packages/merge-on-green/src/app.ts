64b58646e349b31b8f8d928b4a2c5c13e7a30d2b
Jeff Ching <chingor@google.com>
2021-06-18 16:58:16 -0700

    update to gcf-utils 11.1.0 for maxCronRetries=0

--- packages/merge-on-green/src/app.ts
+++ packages/merge-on-green/src/app.ts
@@ -17,8 +17,5 @@ import appFn from './merge-on-green';
 
 const bootstrap = new GCFBootstrapper();
 module.exports['merge_on_green'] = bootstrap.gcf(appFn, {
-  // By default jobs are managed in background cloud tasks queue, we
-  // shouldn't do this for merge on green, as it already retries on a cron:
-  background: false,
-  logging: true,
+  logging: true
 });
