e42aa976f8a57ef5c7a5814e910aeb61703bea54
Jeffrey Rennie <rennie@google.com>
2021-08-10 14:27:49 -0700

    First real test passes.

--- packages/owl-bot/test/core.ts
+++ packages/owl-bot/test/core.ts
@@ -14,21 +14,34 @@
 
 import * as assert from 'assert';
 import {core, RegenerateArgs} from '../src/core';
-import { newFakeOctokitFactory } from './fake-octokit';
+import {
+  FakeIssues,
+  newFakeOctokit,
+  newFakeOctokitFactory,
+} from './fake-octokit';
 
-
-describe('triggerPostProcessBuild()', function() {
-    it('works', async () => {
-        const octokitFactory = newFakeOctokitFactory();
-        const args: RegenerateArgs = {
-            branch: 'test-branch',
-            gcpProjectId: 'test-project',
-            buildTriggerId: '42',
-            owner: 'test-owner',
-            prBody: "This is the best pull request ever.",
-            prNumber: 5,
-            repo: 'nodejs-stapler'
-        };
-        await core.triggerRegeneratePullRequest(octokitFactory, args);
-    });
+describe('triggerPostProcessBuild()', () => {
+  it('creates a comment on the pull request when the pull request body is missing a source commit hash', async () => {
+    const issues = new FakeIssues();
+    const octokit = newFakeOctokit(undefined, issues);
+    const args: RegenerateArgs = {
+      branch: 'test-branch',
+      gcpProjectId: 'test-project',
+      buildTriggerId: '42',
+      owner: 'test-owner',
+      prBody: 'This is the best pull request ever.',
+      prNumber: 5,
+      repo: 'nodejs-stapler',
+    };
+    await core.triggerRegeneratePullRequest(
+      newFakeOctokitFactory(octokit),
+      args
+    );
+    assert.strictEqual(issues.comments.length, 1);
+    const comment = issues.comments[0];
+    assert.strictEqual(comment.owner, 'test-owner');
+    assert.strictEqual(comment.repo, 'nodejs-stapler');
+    assert.strictEqual(comment.issue_number, 5);
+    assert.match(comment.body, /.*missing.*hash.*/);
+  });
 });
