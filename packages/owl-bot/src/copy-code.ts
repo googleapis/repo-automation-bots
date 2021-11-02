031f774f0fa9575e4d929a86bd7afd44d345632f
Jeffrey Rennie <rennie@google.com>
2021-11-02 16:27:37 -0700

    fix: look deeper into pull request history (#2831)
    * fix: look deeper into pull request history
    
    Fixes https://github.com/googleapis/repo-automation-bots/issues/2829
    
    * cache repo histories

--- packages/owl-bot/src/copy-code.ts
+++ packages/owl-bot/src/copy-code.ts
@@ -272,7 +272,7 @@ export async function copyCodeAndCreatePullRequest(
   const token = await octokitFactory.getGitHubShortLivedAccessToken();
   // Octokit token may have expired; refresh it.
   const octokit = await octokitFactory.getShortLivedOctokit(token);
-  if (await copyExists(octokit, destRepo, dest.sourceCommitHash)) {
+  if (await copyExists(octokit, destRepo, dest.sourceCommitHash, 100)) {
     return; // Mid-air collision!
   }
 
@@ -538,6 +538,51 @@ export function copyDirs(
 }
 
 /**
+ * Caches issues and pull requests for a repository.
+ *
+ * Specifically designed for scanGoogleapisGenAndCreatePullRequests() and the worst
+ * case scenario
+ * https://github.com/googleapis/googleapis/commit/bcfd386c224fded5f5c5be7a17bc5884a2a52f59
+ * in which this cache reduces the number of github API calls from approximately 2000
+ * to approximately 20.  The github token has a quota of about 5000 requests per hour,
+ * so this cache is essential.
+ */
+export type RepoHistoryCache = Map<
+  string,
+  {data: {number: number; body?: string | null}[]}
+>;
+
+export function newRepoHistoryCache(): RepoHistoryCache {
+  return new Map();
+}
+
+/**
+ * A request to list issues or list pull requests.  Used as cache key.
+ */
+interface Request {
+  owner: string;
+  repo: string;
+  per_page: number;
+  state: 'all';
+  page: number;
+}
+
+/**
+ * Converts Request to a string cache key.
+ */
+function keyFrom(kind: 'pulls' | 'issues', request: Request): string {
+  // Can't stringify the request directly because the order of properties is not
+  // guaranteed to be consistent.
+  return JSON.stringify([
+    kind,
+    request.owner,
+    request.repo,
+    request.per_page,
+    request.page,
+  ]);
+}
+
+/**
  * Searches for copy tags in recent pull requests and commits.  Pull requests
  * created by older versions of Owl Bot do not contain a copy tag; for those
  * pull requests, we search for the commit hash from googleapis-gen.
@@ -545,25 +590,24 @@ export function copyDirs(
  * @param octokit an octokit instance
  * @param destRepo the repo to search
  * @param sourceCommitHash the string to search for
+ * @param searchDepth the depth of pull request and issue histories to search.
  * @returns true if there's a PR or issue with the commit hash exists
  */
 export async function copyExists(
   octokit: OctokitType,
   destRepo: AffectedRepo,
   sourceCommitHash: string,
+  searchDepth: number,
+  cache?: RepoHistoryCache,
   logger = console
 ): Promise<boolean> {
   // I observed octokit.search.issuesAndPullRequests() not finding recent, open
   // pull requests.  So enumerate them.
   const owner = destRepo.repo.owner;
   const repo = destRepo.repo.repo;
-  const pulls = await octokit.pulls.list({
-    owner,
-    repo,
-    per_page: 100,
-    state: 'all',
-  });
+  const per_page = 100;
   const copyTag = copyTagFrom(destRepo.yamlPath, sourceCommitHash);
+  cache = cache ?? newRepoHistoryCache();
 
   // A generic function that finds matches in either pull request or issue
   // bodies.
@@ -587,17 +631,54 @@ export async function copyExists(
     return false;
   };
 
-  if (findInBodies('Pull request', pulls)) return true;
+  for (let page = 1, prsSeen = 0; prsSeen < searchDepth; page += 1) {
+    const request: Request = {
+      owner,
+      repo,
+      per_page,
+      state: 'all',
+      page,
+    };
+    const cacheKey = keyFrom('pulls', request);
+    const cachedPulls = cache.get(cacheKey);
+    let pulls;
+    if (cachedPulls) {
+      pulls = cachedPulls;
+    } else {
+      pulls = await octokit.pulls.list({...request});
+      cache.set(cacheKey, pulls);
+    }
+    if (findInBodies('Pull request', pulls)) return true;
+    prsSeen += pulls.data.length;
+    if (pulls.data.length < per_page) {
+      break; // No more to see.
+    }
+  }
 
   // And enumerate recent issues too.
-  const issues = await octokit.issues.listForRepo({
-    owner,
-    repo,
-    per_page: 100,
-    state: 'all',
-  });
-
-  if (findInBodies('Issue', issues)) return true;
+  for (let page = 1, issuesSeen = 0; issuesSeen < searchDepth; page += 1) {
+    const request: Request = {
+      owner,
+      repo,
+      per_page,
+      state: 'all',
+      page,
+    };
+    const cacheKey = keyFrom('issues', request);
+    const cachedIssues = cache.get(cacheKey);
+    let issues;
+    if (cachedIssues) {
+      issues = cachedIssues;
+    } else {
+      issues = await octokit.issues.listForRepo({...request});
+      cache.set(cacheKey, issues);
+    }
+    if (findInBodies('Issue', issues)) return true;
+    issuesSeen += issues.data.length;
+    if (issues.data.length < per_page) {
+      break; // No more to see.
+    }
+  }
 
   logger.info(`${sourceCommitHash} not found in ${owner}/${repo}.`);
   return false;
--- packages/owl-bot/src/scan-googleapis-gen-and-create-pull-requests.ts
+++ packages/owl-bot/src/scan-googleapis-gen-and-create-pull-requests.ts
@@ -18,6 +18,7 @@ import tmp from 'tmp';
 import {
   copyCodeAndCreatePullRequest,
   copyExists,
+  newRepoHistoryCache,
   toLocalRepo,
 } from './copy-code';
 import {getFilesModifiedBySha} from '.';
@@ -62,11 +63,13 @@ function isCommitHashTooOld(
  * Scans googleapis-gen and creates pull requests in target repos
  * (ex: nodejs-vision) when corresponding code has been updated.
  * @param sourceRepo normally 'googleapis/googlapis-gen'
+ * @param copyExistsSearchDepth how far into past pull requests and issues to search
  */
 export async function scanGoogleapisGenAndCreatePullRequests(
   sourceRepo: string,
   octokitFactory: OctokitFactory,
   configsStore: ConfigsStore,
+  copyExistsSearchDepth: number,
   cloneDepth = 100,
   logger = console
 ): Promise<number> {
@@ -87,6 +90,7 @@ export async function scanGoogleapisGenAndCreatePullRequests(
 
   const todoStack: Todo[] = [];
   let octokit: null | OctokitType = null;
+  const repoHistoryCache = newRepoHistoryCache();
 
   // Search the commit history for commits that still need to be copied
   // to destination repos.
@@ -119,7 +123,16 @@ export async function scanGoogleapisGenAndCreatePullRequests(
         logger.info(
           `Ignoring ${repoFullName} because ${commitHash} is too old.`
         );
-      } else if (!(await copyExists(octokit, repo, commitHash, logger))) {
+      } else if (
+        !(await copyExists(
+          octokit,
+          repo,
+          commitHash,
+          copyExistsSearchDepth,
+          repoHistoryCache,
+          logger
+        ))
+      ) {
         const todo: Todo = {repo, commitHash};
         logger.info(`Pushing todo onto stack: ${todo}`);
         todoStack.push(todo);
