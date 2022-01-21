5ed19ceafec29adc283fac3a43259849aca5955f
Jeffrey Rennie <rennie@google.com>
2022-01-21 11:51:03 -0800

    fix: open a pull request for lock updates that failed
    Fixes #2709

--- packages/owl-bot/src/scan-and-retry-failed-lock-updates.ts
+++ packages/owl-bot/src/scan-and-retry-failed-lock-updates.ts
@@ -26,12 +26,25 @@ const seventyTwoHours = 3 * twentyFourHours;
  * Scans all the cloud builds for the past 24 hours, and retries builds
  * that never succeeded.
  *
- * @param maxTries try building each trigger at most this many times
+ * Context:
+ *   We ran the post processor for a PR that updated .Owlbot.lock.yaml
+ *   and it failed.
+ *
+ * Strategy:
+ *   Retry twice more.
+ *   If the two retries fail too, then open a pull request with the changes
+ *   to .Owlbot.lock.yaml without trying to run the post processor.
+ *   The post processor will automatically run on the pull request and
+ *   the repository owner will see the error.
+ *
+ * @param triggerId the build trigger id for updating lock branches.
+ * @param forceTriggerId the build trigger id for force-updating lock branches.
+ * @param maxTries The maximum number of times to try.
  */
-
 export async function scanAndRetryFailedLockUpdates(
   projectId: string,
   triggerId: string,
+  forceTriggerId: string | undefined,
   maxTries: number,
   logger = console
 ): Promise<void> {
@@ -39,12 +52,14 @@ export async function scanAndRetryFailedLockUpdates(
   const builds = cb.listBuildsAsync({projectId, pageSize: 100});
   const rebuilds = await filterBuildsToRetry(
     triggerId,
+    forceTriggerId,
     maxTries,
     builds,
     logger
   );
-  logger.info(`${rebuilds.length} to rebuild`);
-  for (const build of rebuilds) {
+  // Retry builds that failed once or twice.
+  logger.info(`${rebuilds.retries.length} to rebuild`);
+  for (const build of rebuilds.retries) {
     logger.info(`Triggering rebuild for ${build.name}`);
     logger.debug(build);
     // This Cloud Run job will run once per hour, so we don't have to worry
@@ -55,6 +70,26 @@ export async function scanAndRetryFailedLockUpdates(
       id: build.id,
     });
   }
+  // Open pull requests for builds that failed 3 times.
+  for (const build of rebuilds.forceRetries) {
+    logger.info(`Triggering force build for ${build.name}`);
+    logger.debug(build);
+    await cb.runBuildTrigger({
+      projectId: build.projectId,
+      triggerId: forceTriggerId,
+      source: {
+        projectId: build.source?.repoSource?.projectId,
+        branchName: build.source?.repoSource?.branchName,
+        substitutions: build.substitutions,
+      },
+    });
+  }
+}
+
+/** Return value from filterBuildsToRetry() */
+export interface Rebuilds {
+  retries: google.devtools.cloudbuild.v1.IBuild[];
+  forceRetries: google.devtools.cloudbuild.v1.IBuild[];
 }
 
 /**
@@ -62,27 +97,38 @@ export async function scanAndRetryFailedLockUpdates(
  * the given build trigger.  Returns a list of builds that should be retried
  * because they never succeeded.
  *
+ * @param triggerId the build trigger id for updating lock branches.
+ * @param forceTriggerId the build trigger id for force-updating lock branches.
  * @param maxTries try building each trigger at most this many times
  */
 export async function filterBuildsToRetry(
   triggerId: string,
+  forceTriggerId: string | undefined,
   maxTries: number,
   builds: AsyncIterable<google.devtools.cloudbuild.v1.IBuild>,
   logger = console
-): Promise<google.devtools.cloudbuild.v1.IBuild[]> {
+): Promise<Rebuilds> {
   // An original build and its retry build will share the same substitution
   // values.  So group them by substition values.
   // Map each failure to the number of times it failed.
+  const keyFrom = (triggerId: string, substitutions: Record<string, string>) =>
+    JSON.stringify([triggerId, ...Object.entries(substitutions).sort()]);
   const countsMap: Map<
     string,
     {
+      index: number;
       build: google.devtools.cloudbuild.v1.IBuild;
       successCount: number;
       failureCount: number;
     }
   > = new Map();
   let newestCreateTime: google.protobuf.ITimestamp | null = null;
+  const triggerIds = [triggerId, forceTriggerId].filter(Boolean);
+  let nextIndex = 0;
+  // Collect the number of failures and successes for each build.
   for await (const build of builds) {
+    const index = nextIndex;
+    nextIndex += 1;
     logger.debug(`${build.createTime?.seconds} ${build.name}`);
     if (!newestCreateTime) {
       newestCreateTime = build.createTime!;
@@ -93,40 +139,58 @@ export async function filterBuildsToRetry(
       logger.info('Finished scanning recent builds.');
       break;
     }
-    if (build.buildTriggerId !== triggerId) {
+    if (!triggerIds.includes(build.buildTriggerId ?? '')) {
       continue; // Not a lock update build.
     }
-    const failed =
+    // Record the success or failure in the map.
+    const failureCount =
       build.status === 'FAILURE' ||
       build.status === 'INTERNAL_ERROR' ||
       build.status === 'TIMEOUT'
         ? 1
         : 0;
-    const succeeded = failed ? 0 : 1;
-    const key = JSON.stringify(
-      Object.entries(build.substitutions ?? {}).sort()
-    );
+    const successCount = failureCount ? 0 : 1;
+    const key = keyFrom(build.buildTriggerId ?? '', build.substitutions ?? {});
     const counts = countsMap.get(key);
     if (counts) {
-      counts.failureCount += failed;
-      counts.successCount += succeeded;
+      counts.failureCount += failureCount;
+      counts.successCount += successCount;
     } else if (secondsElapsedSinceCreateTime > twentyFourHours) {
       // A build that happened more than 24 hours ago is not one we should
       // retry.
     } else {
-      countsMap.set(key, {
-        build,
-        failureCount: failed,
-        successCount: succeeded,
-      });
+      countsMap.set(key, {index, build, failureCount, successCount});
     }
   }
-  return [...countsMap.values()]
-    .filter(
-      counts =>
-        counts.failureCount > 0 &&
-        counts.successCount === 0 &&
-        counts.failureCount < maxTries
-    )
-    .map(counts => counts.build);
+  // Compile the result.
+  const rebuilds: Rebuilds = {forceRetries: [], retries: []};
+  const countsList = [...countsMap.values()].sort(
+    (a, b) => cmp(a.index, b.index) // Sort so order is deterministic.
+  );
+  for (const counts of countsList) {
+    if (counts.failureCount > 0 && counts.successCount === 0) {
+      if (counts.failureCount < maxTries) {
+        // We haven't yet exhausted regular retries, so retry it.
+        rebuilds.retries.push(counts.build);
+      } else if (
+        forceTriggerId &&
+        !countsMap.has(
+          keyFrom(forceTriggerId, counts.build.substitutions ?? {})
+        )
+      ) {
+        // All the regular retries failed.  Force one.
+        rebuilds.forceRetries.push(counts.build);
+      }
+    }
+  }
+  return rebuilds;
+}
+
+// I can't believe this function isn't somewhere in the standard library.
+function cmp(a: number, b: number) {
+  if (a < b) {
+    return -1;
+  } else {
+    return b < a ? 1 : 0;
+  }
 }
