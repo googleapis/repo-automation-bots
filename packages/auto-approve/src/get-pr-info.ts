785db4105a3ce12f1ebc18ae70b98949ba23b7fc
Sofia Leon <sofialeon@google.com>
2022-02-15 12:33:51 -0800

    revert get-pr-info changes

--- packages/auto-approve/src/get-pr-info.ts
+++ packages/auto-approve/src/get-pr-info.ts
@@ -52,10 +52,6 @@ export async function getChangedFiles(
       logger.error(
         `Not found error, ${err.status}, ${err.message} for ${owner}/${repo}/${prNumber}`
       );
-    } else if (err.status === 503) {
-      logger.error(
-        `${err.status}, ${err.message} for ${owner}/${repo}/${prNumber}, continuing to check`
-      );
     }
     throw new Error(
       `${err.status}, ${err.message} for ${owner}/${repo}/${prNumber}`
