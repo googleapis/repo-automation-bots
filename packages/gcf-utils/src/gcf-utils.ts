a9577ba63ef1abe4bbb4ad5147c0e1ce9d261324
Takashi Matsuo <tmatsuo@google.com>
2022-03-31 14:09:43 -0700

    feat(gcf-utils): add an oicd token of the task caller service account (#3346)
    * feat(gcf-utils): add an oicd token of the task caller service account
    
    towards #3344
    
    I'll test this with the canary bot.
    
    * chore: lint fix

--- packages/gcf-utils/src/gcf-utils.ts
+++ packages/gcf-utils/src/gcf-utils.ts
@@ -59,6 +59,8 @@ const SCHEDULER_EVENT_NAMES = [
   SCHEDULER_REPOSITORY_EVENT_NAME,
 ];
 const RUNNING_IN_TEST = process.env.NODE_ENV === 'test';
+const DEFAULT_TASK_CALLER =
+  'task-caller@repo-automation-bots.iam.gserviceaccount.com';
 
 type BotEnvironment = 'functions' | 'run';
 
@@ -216,6 +218,7 @@ interface BootstrapperOptions {
   payloadBucket?: string;
   taskTargetEnvironment?: BotEnvironment;
   taskTargetName?: string;
+  taskCaller?: string;
 }
 
 function defaultTaskEnvironment(): BotEnvironment {
@@ -234,6 +237,7 @@ export class GCFBootstrapper {
   payloadBucket: string | undefined;
   taskTargetEnvironment: BotEnvironment;
   taskTargetName: string;
+  taskCaller: string;
 
   constructor(options?: BootstrapperOptions) {
     options = {
@@ -274,6 +278,7 @@ export class GCFBootstrapper {
     this.location = options.location;
     this.payloadBucket = options.payloadBucket;
     this.taskTargetName = options.taskTargetName || this.functionName;
+    this.taskCaller = options.taskCaller || DEFAULT_TASK_CALLER;
   }
 
   async loadProbot(
@@ -1019,6 +1024,9 @@ export class GCFBootstrapper {
             },
             url,
             body: Buffer.from(payload),
+            oidcToken: {
+              serviceAccountEmail: this.taskCaller,
+            },
           },
         },
       });
@@ -1036,6 +1044,9 @@ export class GCFBootstrapper {
               'Content-Type': 'application/json',
             },
             url,
+            oidcToken: {
+              serviceAccountEmail: this.taskCaller,
+            },
           },
         },
       });
