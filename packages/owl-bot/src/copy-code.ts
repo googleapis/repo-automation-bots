1ba7f239c4641985205ca2a69b892124b6551ceb
Jeffrey Rennie <rennie@google.com>
2022-04-26 15:16:23 -0700

    fix: accelerate coping code into monorepos
    by only globbing the directory structures once.

--- packages/owl-bot/src/copy-code.ts
+++ packages/owl-bot/src/copy-code.ts
@@ -172,6 +172,8 @@ export async function copyCodeIntoCommit(
 
   // Copy code according to each yaml.
   const result = [];
+  const allSourcePaths = globGitRepo(params.sourceRepo);
+  const allDestPaths = globGitRepo(params.destDir);
   for (const yamlPath of yamlPaths) {
     const copyTag = copyTagFrom(yamlPath, params.sourceRepoCommitHash);
     const localYamlPath = path.join(params.destDir, yamlPath);
@@ -182,7 +184,14 @@ export async function copyCodeIntoCommit(
       await reportError(e, yamlPath);
       continue;
     }
-    copyDirs(params.sourceRepo, params.destDir, yaml, logger);
+    copyFiles(
+      params.sourceRepo,
+      allSourcePaths,
+      params.destDir,
+      allDestPaths,
+      yaml,
+      logger
+    );
     result.push({yamlPath, yaml, copyTag});
   }
 
@@ -809,6 +818,15 @@ export function stat(path: string): fs.Stats | undefined {
   }
 }
 
+// glob all the files and dirs in a git repo directory,
+function globGitRepo(repoDir: string): string[] {
+  return glob.sync('**', {
+    cwd: repoDir,
+    dot: true,
+    ignore: ['.git', '.git/**'],
+  });
+}
+
 /**
  * Copies directories and files specified by yaml.
  * @param sourceDir the path to the source repository directory
@@ -818,6 +836,32 @@ export function stat(path: string): fs.Stats | undefined {
 export function copyDirs(
   sourceDir: string,
   destDir: string,
+  yaml: OwlBotYaml,
+  logger: Logger = console
+): void {
+  return copyFiles(
+    sourceDir,
+    globGitRepo(sourceDir),
+    destDir,
+    globGitRepo(destDir),
+    yaml,
+    logger
+  );
+}
+
+/**
+ * Copies directories and files specified by yaml.
+ * @param sourceDir the path to the source repository directory
+ * @param allSourcePaths the list of all the files and directories in the source directory.
+ * @param destDir the path to the dest repository directory.
+ * @param allDestPaths the list of all the files and directories in the dest directory.
+ * @param yaml the OwlBot.yaml file from the dest repository.
+ */
+function copyFiles(
+  sourceDir: string,
+  allSourcePaths: string[],
+  destDir: string,
+  allDestPaths: string[],
   yaml: OwlBotYaml,
   logger: Logger = console
 ): void {
@@ -836,11 +880,6 @@ export function copyDirs(
 
   // Wipe out the existing contents of the dest directory.
   const deadPaths: string[] = [];
-  const allDestPaths = glob.sync('**', {
-    cwd: destDir,
-    dot: true,
-    ignore: ['.git', '.git/**'],
-  });
   for (const rmDest of yaml['deep-remove-regex'] ?? []) {
     if (rmDest && stat(destDir)) {
       const rmRegExp = toFrontMatchRegExp(rmDest);
@@ -878,11 +917,6 @@ export function copyDirs(
   }
 
   // Copy the files from source to dest.
-  const allSourcePaths = glob.sync('**', {
-    cwd: sourceDir,
-    dot: true,
-    ignore: ['.git', '.git/**'],
-  });
   for (const deepCopy of yaml['deep-copy-regex'] ?? []) {
     const regExp = toFrontMatchRegExp(deepCopy.source);
     const sourcePathsToCopy = allSourcePaths.filter(path =>
