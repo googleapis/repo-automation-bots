8ba9c9f36cfc2aa282300aca04126717b4880bb2
Jeff Ching <chingor@google.com>
2022-05-12 09:39:17 -0700

    chore: fix lint

--- packages/release-please/src/release-please.ts
+++ packages/release-please/src/release-please.ts
@@ -145,7 +145,7 @@ async function getConfigWithDefaultBranch(
     octokit,
     owner,
     repo,
-    WELL_KNOWN_CONFIGURATION_FILE,
+    WELL_KNOWN_CONFIGURATION_FILE
   );
   if (config && !config.primaryBranch) {
     config.primaryBranch =
@@ -443,7 +443,7 @@ const handler = (app: Probot) => {
       context.octokit,
       owner,
       repo,
-      WELL_KNOWN_CONFIGURATION_FILE,
+      WELL_KNOWN_CONFIGURATION_FILE
     );
 
     // If no configuration is specified,
@@ -483,7 +483,7 @@ const handler = (app: Probot) => {
       context.octokit,
       owner,
       repo,
-      WELL_KNOWN_CONFIGURATION_FILE,
+      WELL_KNOWN_CONFIGURATION_FILE
     );
 
     // If no configuration is specified,
@@ -528,7 +528,7 @@ const handler = (app: Probot) => {
       context.octokit,
       owner,
       repo,
-      WELL_KNOWN_CONFIGURATION_FILE,
+      WELL_KNOWN_CONFIGURATION_FILE
     );
 
     // If no configuration is specified,
