c7d4c69c76c75c1b22c6007926f6b63c6255a869
sofisl <55454395+sofisl@users.noreply.github.com>
2022-06-16 14:54:00 -0700

    fix: small tweaks in production to get owlbot-bootstrapper running, pass GH token as env variable, redact GH token from logging (#3944)
    * fix: small tweaks in production to get owlbot-bootstrapper running
    
    * pass GH Token as env variable
    * redact GH token

--- packages/owlbot-bootstrapper/common-container/test/utils.test.ts
+++ packages/owlbot-bootstrapper/common-container/test/utils.test.ts
@@ -93,9 +93,11 @@ describe('common utils tests', async () => {
     assert.ok(stdoutBranch.includes(branchName));
   });
 
-  it('should open an issue on a given repo', async () => {
+  it('should open an issue on a given repo, and should not print any GH tokens', async () => {
+    let issueSnapshot: any;
     const scope = nock('https://api.github.com')
       .post(`/repos/${ORG}/googleapis/issues`, body => {
+        issueSnapshot = body.toString();
         snapshot(body);
         return true;
       })
@@ -108,8 +110,11 @@ describe('common utils tests', async () => {
       '1234',
       'myproject',
       'python',
-      'We are missing this piece of critical info'
+      'We are missing this piece of critical info, you used ghs_12346578'
     );
+
+    //eslint-disable-next-line no-useless-escape
+    assert.ok(!issueSnapshot.match(/ghs_[\w\d]*[^@:\/\.]/g));
     scope.done();
   });
 
