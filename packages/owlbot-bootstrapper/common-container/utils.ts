1be2aa47e421b3de5a417abe92276158f709173c
Sofia Leon <sofialeon@google.com>
2022-07-14 19:24:24 -0700

    add https

--- packages/owlbot-bootstrapper/common-container/utils.ts
+++ packages/owlbot-bootstrapper/common-container/utils.ts
@@ -72,7 +72,7 @@ export async function getPRText(octokit: Octokit, owlbotYamlPath: string) {
   const copyTagInfo = `{"p":"${owlbotYamlPath}","h":"${latestSha}"}`;
   const copyTagInfoEncoded = Buffer.from(copyTagInfo).toString('base64');
 
-  return `${REGENERATE_CHECKBOX_TEXT}\nSource-Link: googleapis/googleapis-gen@${latestSha}\nCopy-Tag:\n${copyTagInfoEncoded}`;
+  return `${REGENERATE_CHECKBOX_TEXT}\nSource-Link: https://googleapis/googleapis-gen@${latestSha}\nCopy-Tag:\n${copyTagInfoEncoded}`;
 }
 
 /**
