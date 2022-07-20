be75eb110850a83deb15bccd21112f14c01502aa
Jeffrey Rennie <rennie@google.com>
2022-07-20 17:53:57 +0000

    fix: remove line length checks that are not part of the conventional commit specification.

--- packages/conventional-commit-lint/test/conventional-commit-lint.ts
+++ packages/conventional-commit-lint/test/conventional-commit-lint.ts
@@ -251,37 +251,13 @@ describe('ConventionalCommitLint', () => {
     });
   });
 
-  it('sets a "success" context on PR, if the body is less than 256 in length', async () => {
+  it('sets a "success" context on PR with very long lines', async () => {
     const payload = require(resolve(
       fixturesPath,
       './pull_request_synchronize'
     ));
     const validCommits = [
       ...require(resolve(fixturesPath, './commit_with_long_body_line_length')),
-    ];
-
-    const requests = nock('https://api.github.com')
-      .get('/repos/bcoe/test-release-please/pulls/11/commits?per_page=100')
-      .reply(200, validCommits)
-      .get('/repos/bcoe/test-release-please/pulls/11')
-      .reply(200, pr11)
-      .post('/repos/bcoe/test-release-please/check-runs', body => {
-        snapshot(body);
-        return true;
-      })
-      .reply(200);
-
-    await probot.receive({name: 'pull_request', payload, id: 'abc123'});
-    requests.done();
-  });
-
-  it('sets a "success" context on PR, if the footer is less than 256 in length', async () => {
-    const payload = require(resolve(
-      fixturesPath,
-      './pull_request_synchronize'
-    ));
-    const validCommits = [
-      ...require(resolve(fixturesPath, './commit_with_long_footer_length')),
     ];
 
     const requests = nock('https://api.github.com')
