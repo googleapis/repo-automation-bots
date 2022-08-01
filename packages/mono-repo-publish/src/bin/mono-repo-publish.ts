a548b94da11fccb209875bdb9c3900072b02749a
Jeff Ching <chingor@google.com>
2022-08-01 13:42:33 -0700

    refactor: convert mono-repo-publish to typescript

---
+++ packages/mono-repo-publish/src/bin/mono-repo-publish.ts
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import * as yargs from 'yargs';
+import * as core from '../main';
+
+interface Args {
+  prUrl: string;
+  dryRun: boolean;
+}
+const command: yargs.CommandModule<{}, Args> = {
+  command: '$0',
+  describe: 'publish packages affected by a pull request',
+  builder(yargs) {
+    return yargs
+      .option('pr-url', {
+        describe:
+          'the URL of the GH PR for submodules you wish to publish, e.g., https://github.com/googleapis/release-please/pull/707',
+        type: 'string',
+        demand: true,
+      })
+      .option('dry-run', {
+        describe: 'whether or not to publish in dry run',
+        type: 'boolean',
+        default: false,
+      });
+  },
+  async handler(argv) {
+    const appIdPath = process.env.APP_ID_PATH;
+    const privateKeyPath = process.env.GITHUB_PRIVATE_KEY_PATH;
+    const installationIdPath = process.env.INSTALLATION_ID_PATH;
+
+    if (!appIdPath || !privateKeyPath || !installationIdPath) {
+      throw Error(
+        'Need to set all of APP_ID_PATH, GITHUB_PRIVATE_KEY_PATH, INSTALLATION_ID_PATH'
+      );
+    }
+    const pr = core.parseURL(argv.prUrl);
+    const octokit = core.getOctokitInstance(
+      appIdPath,
+      privateKeyPath,
+      installationIdPath
+    );
+    if (!pr) {
+      throw Error(`Could not find PR from ${argv.prUrl}`);
+    }
+    const files = await core.getsPRFiles(pr, octokit);
+    const submodules = core.listChangedSubmodules(files);
+    const errors = core.publishSubmodules(submodules, argv.dryRun);
+    if (errors.length) {
+      throw Error('some publications failed, see logs');
+    }
+  },
+};
+
+// Get testing repo that touches submodules that we would want to publish
+// Once we have the list, actually calling npm publish on those modules
+yargs
+  .parserConfiguration({'camel-case-expansion': true})
+  .command(command)
+  .parse();
---
+++ packages/mono-repo-publish/test/main.test.ts
@@ -0,0 +1,154 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import * as core from '../src/main';
+import * as assert from 'assert';
+import {describe, it, afterEach} from 'mocha';
+import nock from 'nock';
+import * as sinon from 'sinon';
+import * as path from 'path';
+import {Octokit} from '@octokit/rest';
+
+const sandbox = sinon.createSandbox();
+
+const changedFiles = require(path.resolve(
+  './test/fixtures/pull-request-payloads/many-files.json'
+)) as {filename: string}[];
+nock.disableNetConnect();
+
+// TODO: Add a system-test with a fixture directory with a few fake modules (and a fake PR) and confirm that
+// it tries to publish all the modules (end-to-end test)
+describe('mono-repo publish', () => {
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('should parse owner, user, and number from the URL', () => {
+    const parsed = core.parseURL(
+      'https://github.com/googleapis/release-please/pull/707'
+    );
+    assert.deepStrictEqual(parsed, {
+      owner: 'googleapis',
+      repo: 'release-please',
+      number: 707,
+    });
+  });
+
+  it('should get authenticated Octokit instance', () => {
+    const octokit = core.getOctokitInstance(
+      './test/fixtures/app-id',
+      './test/fixtures/installation-id',
+      './test/fixtures/private-key'
+    );
+
+    assert.ok(octokit instanceof Octokit);
+  });
+
+  it('lists all files on a PR', async () => {
+    const octokit = new Octokit();
+    const fileRequest = nock('https://api.github.com')
+      .get('/repos/testOwner/testRepo/pulls/1/files')
+      .reply(200, changedFiles);
+    const files = await core.getsPRFiles(
+      {owner: 'testOwner', repo: 'testRepo', number: 1},
+      octokit
+    );
+    fileRequest.done();
+    assert.deepStrictEqual(
+      files,
+      changedFiles.map(l => l.filename)
+    );
+  });
+
+  it('identifies submodules that have been changed', () => {
+    const submodules = core.listChangedSubmodules(
+      changedFiles.map(l => l.filename)
+    );
+    assert.deepStrictEqual(submodules, [
+      'packages/firstPackage',
+      'packages/secondPackage',
+      '.',
+    ]);
+  });
+
+  it('passes in the right arguments for npm publish', () => {
+    const execSync = sandbox.spy();
+    core.publishSubmodules(['foo'], false, execSync);
+    sandbox.assert.calledWith(
+      execSync.firstCall,
+      'npm i --registry=https://registry.npmjs.org'
+    );
+    sandbox.assert.calledWith(
+      execSync.secondCall,
+      'npm publish --access=public'
+    );
+  });
+
+  it('passes in --dry-run option', () => {
+    const execSync = sandbox.spy();
+    core.publishSubmodules(['foo'], true, execSync);
+    sandbox.assert.calledWith(
+      execSync.firstCall,
+      'npm i --registry=https://registry.npmjs.org'
+    );
+    sandbox.assert.calledWith(
+      execSync.secondCall,
+      'npm publish --access=public --dry-run'
+    );
+  });
+
+  // A node_modules folder existing in the root directory was preventing
+  // google-api-nodejs-client from publishing.
+  it('it removes node_modules after publish', () => {
+    const execSync = sandbox.spy();
+    const rmdirSync = sandbox.spy();
+    const errors = core.publishSubmodules(['foo'], true, execSync, rmdirSync);
+    sandbox.assert.calledWith(
+      execSync.firstCall,
+      'npm i --registry=https://registry.npmjs.org'
+    );
+    sandbox.assert.calledWith(
+      execSync.secondCall,
+      'npm publish --access=public --dry-run'
+    );
+    sandbox.assert.calledWith(rmdirSync, 'foo/node_modules', {
+      force: true,
+      recursive: true,
+    });
+    assert.strictEqual(errors.length, 0);
+  });
+
+  it('returns array of errors after attempting all publications', () => {
+    const execSync = sandbox.mock().throws(Error('publish fail'));
+    const errors = core.publishSubmodules(['foo'], true, execSync);
+    sandbox.assert.calledWith(
+      execSync.firstCall,
+      'npm i --registry=https://registry.npmjs.org'
+    );
+    assert.strictEqual(errors.length, 1);
+  });
+
+  it('uses npm ci, if package-lock.json exists', () => {
+    const execSync = sandbox.spy();
+    core.publishSubmodules(['test/fixtures'], false, execSync);
+    sandbox.assert.calledWith(
+      execSync.firstCall,
+      'npm ci --registry=https://registry.npmjs.org'
+    );
+    sandbox.assert.calledWith(
+      execSync.secondCall,
+      'npm publish --access=public'
+    );
+  });
+});
