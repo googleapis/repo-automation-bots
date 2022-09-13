c5c988c61bf1c69e3d9ce41650e37b3e39139e73
Takashi Matsuo <tmatsuo@google.com>
2022-09-13 11:23:41 -0700

    chore: fix lint

--- packages/gcf-utils/src/bin/genkey.ts
+++ packages/gcf-utils/src/bin/genkey.ts
@@ -35,48 +35,47 @@ async function run(
   await create(smclient, project, botname, blob);
 }
 
-
 (async () => {
   const argv = await yargs.command(
-      'gen',
-      'Create and upload a client key.',
-      (yargs: Argv) => {
-	return yargs
-	    .option('keyfile', {
-              describe: 'The path to the .pem keyfile',
-              type: 'string',
-              default: 'key.pem',
-	    })
-	    .option('verbose', {
-              alias: 'v',
-              default: false,
-              type: 'boolean',
-	    })
-	    .option('project', {
-              describe: 'Name of GCP project',
-              alias: 'p',
-              type: 'string',
-              demand: true,
-	    })
-	    .option('bot', {
-              alias: 'b',
-              type: 'string',
-              demand: true,
-              description: 'Name of the bot',
-	    })
-	    .option('id', {
-              alias: 'i',
-              type: 'string',
-              demand: true,
-              description: 'ID of the GitHub Application',
-	    })
-	    .option('secret', {
-              alias: 's',
-              type: 'string',
-              demand: true,
-              description: 'Webhook Secret of the GitHub Application',
-	    });
-      }
+    'gen',
+    'Create and upload a client key.',
+    (yargs: Argv) => {
+      return yargs
+        .option('keyfile', {
+          describe: 'The path to the .pem keyfile',
+          type: 'string',
+          default: 'key.pem',
+        })
+        .option('verbose', {
+          alias: 'v',
+          default: false,
+          type: 'boolean',
+        })
+        .option('project', {
+          describe: 'Name of GCP project',
+          alias: 'p',
+          type: 'string',
+          demand: true,
+        })
+        .option('bot', {
+          alias: 'b',
+          type: 'string',
+          demand: true,
+          description: 'Name of the bot',
+        })
+        .option('id', {
+          alias: 'i',
+          type: 'string',
+          demand: true,
+          description: 'ID of the GitHub Application',
+        })
+        .option('secret', {
+          alias: 's',
+          type: 'string',
+          demand: true,
+          description: 'Webhook Secret of the GitHub Application',
+        });
+    }
   ).argv;
 
   const keyfile = argv.keyfile || 'key.pem';
