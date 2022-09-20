ac9d7c49f1700d2560dc890b597933d02a5f18f5
Anthonios Partheniou <partheniou@google.com>
2022-09-20 16:40:23 -0400

    fix(owlbot-bootstrapper): drop support for split repo (#4414)
    * fix(owlbot-bootstrapper): drop support for split repo
    Co-authored-by: sofisl <55454395+sofisl@users.noreply.github.com>

--- packages/owlbot-bootstrapper/common-container/__snapshots__/utils.test.js
+++ packages/owlbot-bootstrapper/common-container/__snapshots__/utils.test.js
@@ -4,10 +4,3 @@ exports['common utils tests opens a PR against the main branch 1'] = {
   title: 'feat: add initial files for google.cloud.kms.v1',
   body: 'Source-Link: https://googleapis/googleapis-gen@6dcb09b5b57875f334f61aebed695e2e4193db5e\nCopy-Tag: eyJwIjoicGFja2FnZXMvZ29vZ2xlLWNsb3VkLWttcy8uZ2l0aHViLy5Pd2xCb3QueWFtbCIsImgiOiI2ZGNiMDliNWI1Nzg3NWYzMzRmNjFhZWJlZDY5NWUyZTQxOTNkYjVlIn0=',
 };
-
-exports[
-  'common utils tests should open an issue on a given repo, and should not print any GH tokens 1'
-] = {
-  title: 'Owlbot Bootstrapper failed creating google.cloud.kms.v1 for python',
-  body: 'Check build number 1234 in myproject for more details:\n\nWe are missing this piece of critical info, you used ',
-};
