9510a0529cd0f1bcbbf835b4b665de26b5029f34
Sofia Leon <sofialeon@google.com>
2022-10-12 13:59:54 -0700

    update package.json

--- packages/owlbot-bootstrapper/common-container/fetch-api-info.ts
+++ packages/owlbot-bootstrapper/common-container/fetch-api-info.ts
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {Octokit} from '@octokit/rest';
 import {Storage} from '@google-cloud/storage';
 import {ServiceConfigYaml, DriftApi, ReleaseLevel} from './interfaces';
 import yaml from 'js-yaml';
