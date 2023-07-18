3c8849fc021e618aad2d0b9c9f9c51fd53bac663
sofisl <55454395+sofisl@users.noreply.github.com>
2023-07-18 13:52:31 -0700

    fix(auto-approve): regex typo for Docker dependency (#5152)
    Fixes https://github.com/googleapis/repo-automation-bots/issues/5097

--- packages/auto-approve/src/process-checks/sample-application-repos/docker-dependency.ts
+++ packages/auto-approve/src/process-checks/sample-application-repos/docker-dependency.ts
@@ -58,7 +58,7 @@ export class DockerDependency extends BaseLanguageRule {
       ),
       // This would match: '+FROM cypress/included:12.11.0@sha256:29dfeed99db7a9678a4402f9175c98074c23bbb5ad109058702bc401fc3cdd02'
       newVersion: new RegExp(
-        /\+FROM[\s](\D*):([0-9])*\.([0-9]*\.[0-9]*).*@sha256:([a-z0-9]{64})/
+        /\+FROM[\s](\D*):([0-9]*)\.([0-9]*\.[0-9]*).*@sha256:([a-z0-9]{64})/
       ),
     },
   ];
