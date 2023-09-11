057125e0fe11d6a6b6b2498e27a1cdb87b404593
Jeff Ching <chingor@google.com>
2023-09-11 09:43:57 -0700

    refactor: use sinon to mock octokit methods (#5195)
    feat!: require node 18
    feat!: remove `addOrUpdateIssueComment` which is moved to issue-utils.

--- packages/gcf-utils/test/gcf-utils.ts
+++ packages/gcf-utils/test/gcf-utils.ts
@@ -14,28 +14,12 @@
 
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-import {
-  addOrUpdateIssueComment,
-  getAuthenticatedOctokit,
-  ERROR_REPORTING_TYPE_NAME,
-  logErrors,
-} from '../src/gcf-utils';
+import {ERROR_REPORTING_TYPE_NAME, logErrors} from '../src/gcf-utils';
 import {GCFLogger} from '../src/logging/gcf-logger';
-import * as gcfUtilsModule from '../src/gcf-utils';
-import {Octokit} from '@octokit/rest';
-import {resolve} from 'path';
-import snapshot from 'snap-shot-it';
 import {ObjectWritableMock} from 'stream-mock';
-import {Probot, ProbotOctokit} from 'probot';
-import {describe, beforeEach, afterEach, it} from 'mocha';
-import nock from 'nock';
-import * as sinon from 'sinon';
+import {describe, beforeEach, it} from 'mocha';
 import * as assert from 'assert';
 import {LogLine} from './test-helpers';
-
-nock.disableNetConnect();
-
-const fixturesPath = resolve(__dirname, '../../test/fixtures');
 
 describe('logErrors', () => {
   let destination: ObjectWritableMock;
@@ -62,115 +46,5 @@ describe('logErrors', () => {
     logErrors(logger, new Error('An error happened'));
     const loggedLines: LogLine[] = readLogsAsObjects(destination);
     assert.strictEqual(loggedLines[0]['@type'], ERROR_REPORTING_TYPE_NAME);
-  });
-});
-
-// Test app
-const app = (app: Probot) => {
-  app.on('issues', async context => {
-    const octokit = await getAuthenticatedOctokit(
-      context.payload.installation?.id
-    );
-    await addOrUpdateIssueComment(
-      octokit,
-      context.payload.repository.owner.login,
-      context.payload.repository.name,
-      context.payload.issue.number,
-      context.payload.installation!.id,
-      'test comment',
-      context.payload.issue.title === 'onlyUpdate'
-    );
-  });
-};
-
-describe('gcf-utils', () => {
-  describe('addOrUpdateIssueComment', () => {
-    const sandbox = sinon.createSandbox();
-    let getAuthenticatedOctokitStub: sinon.SinonStub;
-    let probot: Probot;
-    beforeEach(() => {
-      probot = new Probot({
-        appId: 1,
-        githubToken: 'test',
-        Octokit: ProbotOctokit.defaults({
-          retry: {enabled: false},
-          throttle: {enabled: false},
-        }),
-      });
-      probot.load(app);
-      getAuthenticatedOctokitStub = sandbox.stub(
-        gcfUtilsModule,
-        'getAuthenticatedOctokit'
-      );
-      getAuthenticatedOctokitStub.resolves(new Octokit());
-    });
-
-    afterEach(() => {
-      nock.cleanAll();
-      sandbox.restore();
-    });
-
-    it('creates a comment', async () => {
-      const payload = require(resolve(fixturesPath, './issue_event'));
-      const requests = nock('https://api.github.com')
-        .get(
-          '/repos/tmatsuo/python-docs-samples/issues/10/comments?per_page=50'
-        )
-        .reply(200, [])
-        .post('/repos/tmatsuo/python-docs-samples/issues/10/comments', body => {
-          snapshot(body);
-          return true;
-        })
-        .reply(200);
-
-      await probot.receive({
-        name: 'issues',
-        payload,
-        id: 'test',
-      });
-      requests.done();
-    });
-    it('does not create a comment', async () => {
-      const payload = require(resolve(
-        fixturesPath,
-        './issue_only_update_event'
-      ));
-      const requests = nock('https://api.github.com')
-        .get(
-          '/repos/tmatsuo/python-docs-samples/issues/10/comments?per_page=50'
-        )
-        .reply(200, []);
-
-      await probot.receive({
-        name: 'issues',
-        payload,
-        id: 'test',
-      });
-      requests.done();
-    });
-    it('updates a comment', async () => {
-      const payload = require(resolve(fixturesPath, './issue_event'));
-      const listCommentsResponse = require(resolve(
-        fixturesPath,
-        './list_issue_comments'
-      ));
-      const requests = nock('https://api.github.com')
-        .get(
-          '/repos/tmatsuo/python-docs-samples/issues/10/comments?per_page=50'
-        )
-        .reply(200, listCommentsResponse)
-        .patch('/repos/tmatsuo/python-docs-samples/issues/comments/1', body => {
-          snapshot(body);
-          return true;
-        })
-        .reply(200);
-
-      await probot.receive({
-        name: 'issues',
-        payload,
-        id: 'test',
-      });
-      requests.done();
-    });
   });
 });
