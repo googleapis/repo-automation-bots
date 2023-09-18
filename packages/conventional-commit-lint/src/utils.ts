3ffb6b26a39a038088355e8e634eeef098a5f29a
Benjamin E. Coe <bencoe@google.com>
2023-09-18 13:10:33 -0400

    fix: handle PR with greater than 100 commits (#5189)
    Fixes #5188
    
    ---------
    
    Co-authored-by: Jeff Ching <chingor@google.com>
    Co-authored-by: Jeffrey Rennie <rennie@google.com>

--- packages/conventional-commit-lint/src/utils.ts
+++ packages/conventional-commit-lint/src/utils.ts
@@ -69,9 +69,14 @@ export async function scanPullRequest(
     pull_number: pull_request.number,
   });
 
-  let commits: PullsListCommitsResponseData;
+  const commits: PullsListCommitsResponseData = [];
   try {
-    commits = (await octokit.pulls.listCommits(commitParams)).data;
+    for await (const response of octokit.paginate.iterator(
+      octokit.rest.pulls.listCommits,
+      commitParams
+    )) {
+      commits.push(...response.data);
+    }
   } catch (e) {
     const err = e as Error;
     logger.error(err);
