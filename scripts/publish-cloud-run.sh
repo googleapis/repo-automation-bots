520d69cc6ed68523e397ff705d22b4a64e8e5a57
Jeff Ching <chingor@google.com>
2024-11-20 08:56:14 -0800

    build: simplify deploy scripts (#5563)
    service configuration including task queues are being managed by
    terraform instead
    
    In a follow-up, we should be able to directly run the gcloud deploy
    commands to simplify the release process

--- scripts/publish-cloud-run.sh
+++ scripts/publish-cloud-run.sh
@@ -16,7 +16,7 @@
 set -eo pipefail
 
 if [ $# -lt 6 ]; then
-  echo "Usage: $0 <botDirectory> <projectId> <bucket> <keyLocation> <keyRing> <region> [botName] [timeout] [min-instance] [concurrency]"
+  echo "Usage: $0 <botDirectory> <projectId> <bucket> <keyLocation> <keyRing> <region> [botName]"
   exit 1
 fi
 
@@ -32,35 +32,6 @@ if [ $# -ge 7 ]; then
   botName=$7
 fi
 
-if [ $# -ge 8 ]; then
-  timeout=$8
-else
-  timeout="3600"
-fi
-
-if [ $# -ge 9 ]; then
-  minInstances=$9
-else
-  minInstances="0"
-fi
-
-if [ -z "${CONCURRENCY}" ]; then
-  if [ $# -ge 10 ]; then
-    CONCURRENCY=${10}
-  else
-    CONCURRENCY="80"
-  fi
-fi
-
-if [ "${project}" == "repo-automation-bots" ]; then
-    webhookTmpBucket=tmp-webhook-payloads
-elif [ "${project}" == "repo-automation-bots-staging" ]; then
-    webhookTmpBucket=tmp-webhook-payloads-staging
-else
-    echo "deploying to '${project}' is not supported"
-    exit 1
-fi
-
 if [ -z "${SERVICE_NAME}" ]; then
   SERVICE_NAME=${botName//_/-}
 fi
@@ -70,82 +41,13 @@ if [ -z "${IMAGE_NAME}" ]; then
 fi
 
 pushd "${directoryName}"
-functionName=${botName//-/_}
-queueName=${botName//_/-}
 
 deployArgs=(
   "--image"
   "${IMAGE_NAME}"
-  "--set-env-vars"
-  "DRIFT_PRO_BUCKET=${bucket}"
-  "--set-env-vars"
-  "KEY_LOCATION=${keyLocation}"
-  "--set-env-vars"
-  "KEY_RING=${keyRing}"
-  "--set-env-vars"
-  "GCF_SHORT_FUNCTION_NAME=${functionName}"
-  "--set-env-vars"
-  "PROJECT_ID=${project}"
-  "--set-env-vars"
-  "GCF_LOCATION=${region}"
-  "--set-env-vars"
-  "PUPPETEER_SKIP_CHROMIUM_DOWNLOAD='1'"
-  "--set-env-vars"
-  "WEBHOOK_TMP=${webhookTmpBucket}"
-  "--set-env-vars"
-  "BOT_RUNTIME=run"
   "--platform"
   "managed"
-  "--region"
-  "${region}"
-  "--timeout"
-  "${timeout}"
-  "--min-instances"
-  "${minInstances}"
-  "--concurrency"
-  "${CONCURRENCY}"
   "--quiet"
 )
-if [ -n "${SERVICE_ACCOUNT}" ]; then
-  deployArgs+=( "--service-account" "${SERVICE_ACCOUNT}" )
-fi
-if [ -n "${MEMORY}" ]; then
-  deployArgs+=( "--memory" "${MEMORY}" )
-fi
-if [ -n "${NUMBER_OF_CPU}" ]; then
-  deployArgs+=( "--cpu" "${NUMBER_OF_CPU}" )
-fi
 echo "About to cloud run app ${SERVICE_NAME}"
 gcloud run deploy "${SERVICE_NAME}" "${deployArgs[@]}"
-
-if [ -z "${SECURE_ACCESS}" ]; then
-  echo "Adding ability for allUsers to execute the service"
-  gcloud run services add-iam-policy-binding "${SERVICE_NAME}" \
-    --member="allUsers" \
-    --region "${region}" \
-    --role="roles/run.invoker"
-fi
-
-echo "Deploying Queue ${queueName}"
-
-maxConcurrentDispatches="2048"
-if [ -n "${TASK_MAX_CONCURRENT_DISPATCHES}" ]; then
-  maxConcurrentDispatches="${TASK_MAX_CONCURRENT_DISPATCHES}"
-fi
-
-maxDispatchesPerSecond="500"
-if [ -n "${TASK_MAX_DISPATCHES_PER_SECOND}" ]; then
-  maxDispatchesPerSecond="${TASK_MAX_DISPATCHES_PER_SECOND}"
-fi
-
-verb="create"
-if gcloud tasks queues describe "${queueName}"  &>/dev/null; then
-  verb="update"
-fi
-
-gcloud --quiet tasks queues ${verb} "${queueName}" \
-  --max-concurrent-dispatches="${maxConcurrentDispatches}" \
-  --max-attempts="100" \
-  --max-retry-duration="43200s" \
-  --max-dispatches-per-second="${maxDispatchesPerSecond}" \
-  --min-backoff="5s"
