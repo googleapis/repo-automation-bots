72a746cafadfea131401f20e42f358d09a200ea7
ldetmer <1771267+ldetmer@users.noreply.github.com>
2025-02-21 13:19:43 -0500

    fix: we should using Stringify when calling verify, then it will return appropriate error message

--- packages/gcf-utils/src/gcf-utils.ts
+++ packages/gcf-utils/src/gcf-utils.ts
@@ -445,24 +445,16 @@ export class GCFBootstrapper {
       const botRequest = parseBotRequest(request);
 
       // validate the signature
-      try {
-        if (
-          !wrapConfig.skipVerification &&
-          !(await this.probot.webhooks.verify(
-            request.rawBody ? request.rawBody.toString() : request.body,
-            botRequest.signature
-          ))
-        ) {
-          response.status(400).send({
-            statusCode: 400,
-            body: JSON.stringify({message: 'Invalid signature'}),
-          });
-          return;
-        }
-      } catch (err) {
+      if (
+        !wrapConfig.skipVerification &&
+        !(await this.probot.webhooks.verify(
+          JSON.stringify(request.rawBody ? request.rawBody.toString() : request.body),
+          botRequest.signature
+        ))
+      ) {
         response.status(400).send({
           statusCode: 400,
-          body: JSON.stringify({message: err.message}),
+          body: JSON.stringify({message: 'Invalid signature'}),
         });
         return;
       }
