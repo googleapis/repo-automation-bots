008239ab2d37fe2bd93315ae477476782ced4970
sofisl <55454395+sofisl@users.noreply.github.com>
2026-01-09 15:00:03 -0800

    Add paths to skip for OwlBot configuration

--- packages/owl-bot/src/handlers.ts
+++ packages/owl-bot/src/handlers.ts
@@ -40,6 +40,12 @@ type ListReposResponse = Endpoints['GET /orgs/{org}/repos']['response'];
 // those files.
 export const CONFIG_PATHS_TO_SKIP = [
   'packages/gapic-node-processing/templates/bootstrap-templates/.OwlBot.yaml',
+  'generator/gapic-generator-typescript/test-fixtures/google-cloud-speech-nodejs/speech-v1-nodejs/.OwlBot.yaml',
+  'generator/gapic-generator-typescript/test-fixtures/google-cloud-speech-nodejs/speech-v1p1beta1-nodejs/.OwlBot.yaml',
+  'generator/gapic-generator-typescript/test-fixtures/google-cloud-speech-nodejs/speech-v2-nodejs/.OwlBot.yaml',
+  'generator/gapic-generator-typescript/test-fixtures/google-cloud-speech/.OwlBot.yaml',
+  'generator/gapic-generator-typescript/test-fixtures/google-cloud-tasks-nodejs/tasks-v2-nodejs/.OwlBot.yaml',
+  'generator/gapic-generator-typescript/test-fixtures/google-cloud-tasks-nodejs/tasks-v2beta2-nodejs/.OwlBot.yaml'
 ];
 /**
  * Invoked when a new pubsub message arrives because a new post processor
